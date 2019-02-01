39a7a8690abd70bfcf03e799bb9b2339040db3f6
SantiagoDomiro <dopretocoracao@gmail.com>
2019-02-01 15:55:59 -0600

    removed MFA intructions

--- concatena.R
+++ concatena.R
@@ -10,24 +10,6 @@ concatenadas=lapply(1:5,function(x) sapply(list(mirSubti,Msubti,TMMArsyn),functi
 	y[[which(names(y)==names(shared)[x])]][,substr(colnames(y[[which(names(y)==names(shared)[x])]]),1,12)%in%shared[[x]]]))
 names(concatenadas)=names(shared)
 concatenadas=lapply(concatenadas,function(x) sapply(x,function(y) y[,order(substr(colnames(y),1,12))]))
-save(concatenadas,normal,file="conca/porSubti.RData")
-#########################################
-
-library(parallel)
-library(FactoMineR)
-load("porSubti.RData")
-
-#concatenadas=lapply(concatenadas,function(x) t(do.call(rbind,x))
-# Calculate the number of cores
-no_cores <- detectCores() - 1
-# Initiate cluster
-cl <- makeCluster(no_cores)
-clusterEvalQ(cl,{library(FactoMineR)})
-
-MFAresus=parLapply(cl, concate,function(x) MFA(x,group=c(1588,395806,13904),name.group=c("miR","methy","mRNA"))
-stopCluster(cl)
-save(MFAresus,file="MFA.RData")
-##############################################
 
 i=grep("^rs",rownames(concatenadas$LumA[[2]]),perl=T,invert=T)
 #hay que quitar las probes de SNPs
@@ -36,3 +18,7 @@ concatenadas$Basal[[2]]=concatenadas$Basal[[2]][i,]
 concatenadas$LumB[[2]]=concatenadas$LumB[[2]][i,]
 concatenadas$Her2[[2]]=concatenadas$Her2[[2]][i,]
 concatenadas$normal[[2]]=concatenadas$normal[[2]][i,]
+
+save(concatenadas,file="conca/porSubti.RData")
+#########################################
+
