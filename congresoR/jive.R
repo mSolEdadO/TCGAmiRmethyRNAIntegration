237c87f56ade4ee63b4375a8ec1caab53643f0f9
SantiagoDomiro <dopretocoracao@gmail.com>
2019-02-06 15:52:12 -0600

    ordered matrices so subtype color matches the one in MFA.R

--- congresoR/jive.R
+++ congresoR/jive.R
@@ -1,4 +1,5 @@
 library(r.jive)
+library(ggsci)#pa repetir la paleta de colores de MFA.R
 load("useR/MFA.Rda")
 
 #jive necesita una lista con las distintas omicas
@@ -9,13 +10,20 @@ omics=list(methy,rna,mirna)
 names(omics)=c("methy","rna","mirna")
 subtipos=gsub(".TCGA.+","",colnames(omics$methy))
 
+#hay que ordenar las samples para que los colores por subtipo correspondan con el de MFA.R
+omics$methy=omics$methy[,order(match(subtipos,names(table(subtipos))))]
+omics$rna=omics$rna[,order(match(subtipos,names(table(subtipos))))]
+omics$mirna=omics$mirna[,order(match(subtipos,names(table(subtipos))))]
+subtipos=subtipos[order(match(subtipos,names(table(subtipos))))]
+
+
 JIVEomics=jive(omics)
 #Final joint rank: 2 , final individual ranks: 6 3 4 
 pdf("JIVEomics.pdf")
 showVarExplained(JIVEomics)  
-showPCA(JIVEomics,n_joint=2,Colors=scales::hue_pal()(5)[as.factor(subtipos)],pch=17)
-legend("top",horiz=T,legend=unique(subtipos),fill=scales::hue_pal()(5),bty='n',border=NA)
-showPCA(JIVEomics,n_joint=1,n_indiv=c(1,1,1),Colors=scales::hue_pal()(5)[as.factor(subtipos)],pch=17)
+showPCA(JIVEomics,n_joint=2,Colors=pal_lancet()(5)[as.factor(subtipos)],pch=17)
+legend("top",horiz=T,legend=unique(subtipos),fill=pal_lancet()(5),bty='n',border=NA)
+showPCA(JIVEomics,n_joint=1,n_indiv=c(1,1,1),Colors=pal_lancet()(5)[as.factor(subtipos)],pch=17)
 dev.off()
 
 #pdf("JIVEheatmaps.pdf",height=465,width=705)
