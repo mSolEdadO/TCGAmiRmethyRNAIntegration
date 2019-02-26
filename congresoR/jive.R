0cfb0123d9f467f3a0519f07ba52073df3851a3b
SantiagoDomiro <36210371+SantiagoDomiro@users.noreply.github.com>
2019-02-25 19:23:36 -0600

    updated plots

--- congresoR/jive.R
+++ congresoR/jive.R
@@ -6,20 +6,13 @@ load("subset/subset.Rda")
 omics=list(t(methy),t(rna),t(mirna))
 names(omics)=c("methy","rna","mirna")
 
-#hay que ordenar las samples para que los colores por subtipo correspondan con el de MFA.R
-omics$methy=omics$methy[,order(match(subtipos,names(table(subtipos))))]
-omics$rna=omics$rna[,order(match(subtipos,names(table(subtipos))))]
-omics$mirna=omics$mirna[,order(match(subtipos,names(table(subtipos))))]
-subtipos=subtipos[order(match(subtipos,names(table(subtipos))))]
-
-
 JIVEomics=jive(omics)
-#Final joint rank: 2 , final individual ranks: 6 3 4 
+#Final joint rank: 3 , final individual ranks: 72 45 2 
 pdf("JIVEomics.pdf")
 showVarExplained(JIVEomics)  
-showPCA(JIVEomics,n_joint=2,Colors=pal_lancet()(5)[as.factor(subtipos)],pch=17)
-legend("top",horiz=T,legend=unique(subtipos),fill=pal_lancet()(5),bty='n',border=NA)
-showPCA(JIVEomics,n_joint=1,n_indiv=c(1,1,1),Colors=pal_lancet()(5)[as.factor(subtipos)],pch=17)
+showPCA(JIVEomics,n_joint=2,Colors=pal_lancet()(5)[design$subtype],pch=19)
+legend("top",horiz=T,legend=levels(subtipos),fill=pal_lancet()(5),bty='n',border=NA)
+showPCA(JIVEomics,n_joint=1,n_indiv=c(1,1,1),Colors=pal_lancet()(5)[design$subtype],pch=19)
 dev.off()
 
 #pdf("JIVEheatmaps.pdf",height=465,width=705)
