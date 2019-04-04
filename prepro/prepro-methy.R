9faa11c649c6d55b13f499bf84cc5136590b0315
SantiagoDomiro <36210371+SantiagoDomiro@users.noreply.github.com>
2019-04-04 17:10:06 -0600

    filter noise prone probes

--- prepro/prepro-methy.R
+++ prepro/prepro-methy.R
@@ -1,10 +1,9 @@
 library(TCGAbiolinks)
 library(SummarizedExperiment)
-library(NOISeq)
 library(Venn.Diagram)
 library(doParallel)
 
-#####################################prepare the data########################################
+#######prepare the data########################################
 #reference data
 mthyltN=GDCprepare(mthyltN)
 temp=cbind(colnames(mthyltN),"Illumina Human Methylation 450",substr(colnames(mthyltN),1,12),"normal")
@@ -16,22 +15,14 @@ save(methyDesign,mthyltN,file="GDCdata/TCGA-BRCA/harmonized/DNA_Methylation/meth
 mtC=methyDesign[methyDesign[,4]!="Her2"&methyDesign[,4]!="normal",1]
 mthyltn=GDCquery(project = "TCGA-BRCA",data.category = "DNA Methylation",barcode=mtC)
 methy450=GDCprepare(mthyltn)
-annotMethy=rowData(methy450)
+#annotMethy=rowData(methy450)
 methy450=unlist(assays(methy450))
 nas=rowSums(is.na(methy450))
 #table(nas) hay 365411 probes completas y 89512 sin dato
 methy450=methy450[nas<ncol(methy450),]
 save(annotMethy,methy450,file="GDCdata/TCGA-BRCA/harmonized/DNA_Methylation/methy450.RData")
-#tumor 27 platform data
-#mtC=methyDesign[methyDesign[,4]=="Her2",1]
-#mthyltn=GDCquery(project = "TCGA-BRCA",data.category = "DNA Methylation",barcode=mtC)
-#methy27=GDCprepare(mthyltn)
-#dim: 25978 119 
-#methy27=unlist(assays(methy27))
-#nas=rowSums(is.na(methy27))#hay 22746 probes completas y 119 sin dato
-#methy27=methy27[nas<ncol(methy27),]
-#save(methy27,file="GDCdata/TCGA-BRCA/harmonized/DNA_Methylation/methy27.RData")
-##############################impute missing data########################################
+save(methy450,file="GDCdata/TCGA-BRCA/harmonized/DNA_Methylation/methy450.RData")
+#######impute missing data########################################
 load("GDCdata/TCGA-BRCA/harmonized/DNA_Methylation/methy450.RData")
 load("GDCdata/TCGA-BRCA/harmonized/DNA_Methylation/methyN_ori.RData")
 #load("GDCdata/TCGA-BRCA/harmonized/DNA_Methylation/methy27.RData")
@@ -66,34 +57,65 @@ venn.diagram(x = list(A=methyDesign$patient[methyDesign$shortLetterCode!="NT"],
 col = "transparent", fill = c("cornflowerblue","green"), 
 alpha = 0.50,cex = 1.5, fontface = "bold", label.col="black", 
 cat.cex = 1.5,margin = 0.1,category.names=c("tumor","normal"))
-####################################correct batches#########################################
-colnames(methyDesign)=c("barcode","platform","patient","subtype")
-methyDesign=as.data.frame(methyDesign)
+############filter noise-prone probes######################################
+library(lumi)
+library(minfi)
 
 probes=intersect(intersect(intersect(intersect(rownames(methySubti$Basal),rownames(methySubti$LumA)),rownames(methySubti$LumB)),rownames(methySubti$Her2)),rownames(methySubti$normal))
 methySubti=lapply(methySubti,function(x) x[rownames(x)%in%probes,])
+
+methyDesign=as.data.frame(methyDesign)
+methyDesign=cbind(methyDesign,sapply(strsplit(as.character(methyDesign$barcode),"-"),function(x) substr(x[4],1,2)))
+methyDesign=cbind(methyDesign,sapply(strsplit(as.character(methyDesign$barcode),"-"),function(x) substr(x[4],3,3)))
+methyDesign=cbind(methyDesign,sapply(strsplit(as.character(methyDesign$barcode),"-"),function(x) substr(x[5],1,2)))
+methyDesign=cbind(methyDesign,sapply(strsplit(as.character(methyDesign$barcode),"-"),function(x) substr(x[5],3,3)))
+methyDesign=cbind(methyDesign,sapply(strsplit(as.character(methyDesign$barcode),"-"),function(x) x[6]))
+colnames(methyDesign)=c("barcode","platform","patient","subtype","sample","vial","portion","analyte","plate")
+
+methy=do.call(cbind,methySubti)#395741*814
+methyRgS=makeGenomicRatioSetFromMatrix(mat=methy,rownames=rownames(methy),
+	pData=methyDesign,mergeManifest=T,what="Beta")
+methyRgS=dropLociWithSnps(methyRgS,snps="CpG")#polymorphisms may affect DNAm measurements
+#394391*814 = filtered 1350 rs from dbSNP
+annot=getAnnotation(methyRgS,dropNonMapping=T)#all 394391 probes map to only one locus
+methyRgS=methyRgS[annot$chr!="chrY",]#there're 10/13 male LumB & we don't want a fake DM due to chr imbalance 
+annot=annot[annot$chr!="chrY",]
+methyRgS=methyRgS[annot$chr!="chrX",]
+annot=annot[annot$chr!="chrX",]
+#384575*814
+
 #Beta values have severe heteroscedasticity for highly methylated or unmethylated CpG sites.
 #M-value method provides much better performance in terms of detection rate and true positive rate
 #for both highly methylated and unmethylated CpG sites
-Msubti=lapply(methySubti,function(x) log2(x/(1-x)))
-noiseqData=lapply(Msubti,function(x) readData(data = x, factors = methyDesign[methyDesign$barcode%in%colnames(x),]))
-myPCAsubti = lapply(noiseqData,function(x) dat(x, type = "PCA", norm = T,logtransf=T))
-shared=do.call(cbind,Msubti)
-noiseqData1=readData(data = shared, factors = methyDesign[methyDesign$barcode%in%colnames(shared),])
-myPCAshared=dat(noiseqData1, type = "PCA", norm = T,logtransf=T)
+methySubti=lapply(unique(methyDesign$subtype),function(x) methy[,methyDesign$subtype==x])
+methySubti=lapply(methySubti,function(x) x[,!duplicated(substr(colnames(x),1,12))])
+names(methySubti)=unique(methyDesign$subtype)
+Msubti=lapply(methySubti,beta2m)
+sapply(Msubti,dim)
+#      Basal   LumA   LumB   Her2 normal
+#[1,] 384575 384575 384575 384575 384575
+#[2,]    135    331    177     75     96
+
+pdf("methyDistr.pdf")
+  plot(density(methySubti$Basal),col="blue",frame.plot=F)
+	lines(density(methySubti$LumA),col="purple")
+	lines(density(methySubti$LumB),col="green")
+	lines(density(methySubti$Her2),col="orange")
+	lines(density(methySubti$normal),col="red")
+  plot(density(Msubti$Basal),col="blue",frame.plot=F)
+  	lines(density(Msubti$LumA),col="purple")
+	lines(density(Msubti$LumB),col="green")
+	lines(density(Msubti$Her2),col="orange")
+	lines(density(Msubti$normal),col="red")
+  legend("topright",legend=c("Basal","Her2","LumA","LumB","normal"),fill=c("blue","orange","purple","green","red"),bty="n")
+dev.off()
+#######correct batches#########################################
+library(NOISeq)
+noiseqData=readData(data =do.call(cbind,Msubti), factors =methyDesign)
+myPCAshared=dat(noiseqData, type = "PCA", norm = T,logtransf=T)
 pdf("mPCA.pdf")
-par(mfrow=c(3,2) )
-explo.plot(myPCAsubti$Basal, samples = c(1,2), plottype = "scores", factor = "subtype")
-explo.plot(myPCAsubti$LumA, samples = c(1,2), plottype = "scores", factor = "subtype")
-explo.plot(myPCAsubti$LumB, samples = c(1,2), plottype = "scores", factor = "subtype")
-explo.plot(myPCAsubti$Her2, samples = c(1,2), plottype = "scores", factor = "subtype")
-explo.plot(myPCAsubti$normal, samples = c(1,2), plottype = "scores", factor = "subtype")
-explo.plot(myPCAshared, samples = c(1,2), plottype = "scores", factor = "subtype")
+ explo.plot(myPCAshared, samples = c(1,2), plottype = "scores", factor = "subtype")
+ explo.plot(myPCAshared, samples = c(3,5), plottype = "scores", factor = "plate")
 dev.off()
 
-methySubti=lapply(methySubti,function(x) x[,!duplicated(substr(colnames(x),1,12))])#keep only one sample per patient
-#sapply(methySubti,dim)
-#       LumA  Basal   LumB normal
-#[1,] 395808 395808 395808 395808
-#[2,]    331    135    177     96
-save(methySubti,methyDesign,Msubti,file="imptMethy.RData")
+save(methySubti,methyDesign,Msubti,annot,file="imptMethy.RData")
