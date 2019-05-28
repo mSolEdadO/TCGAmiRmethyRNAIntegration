467ba06e6adb20ea6282bf22084e5cf3356d9660
SantiagoDomiro <dopretocoracao@gmail.com>
2019-05-28 16:07:25 -0500

    improved input by using the MFA normalized matrix

--- miniProy.R
+++ miniProy.R
@@ -1,7 +1,8 @@
 library(biomaRt)  
 library(genefu)
-library(mixOmics)
+library(caret)
 load("data/porSubti.RData")
+load("data/MFAmultio.RData")
 
 mart=useEnsembl("ensembl",dataset="hsapiens_gene_ensembl")
 myannot=getBM(attributes = c("ensembl_gene_id", "hgnc_id","hgnc_symbol"),
@@ -14,32 +15,49 @@ PAM50genes=rbind(PAM50genes,myannot[myannot$hgnc_symbol=="NUF2",])
 PAM50genes=rbind(PAM50genes,myannot[myannot$hgnc_symbol=="NDC80",])
 write.table(PAM50genes,"data/PAM50genes.tsv",sep='\t',quote=F,row.names=F)
 
-expr=concatenadas$normal[[3]][rownames(concatenadas$normal[[3]])%in%PAM50genes$ensembl_gene_id,]
-PAM50genes=PAM50genes[order(match(PAM50genes$ensembl_gene_id,rownames(expr))),]
-rownames(expr)=PAM50genes$hgnc_symbol
+normalEigenScaled=MFAmultio$normal$global.pca$call$X
+#preProcess
+#near-zero-variance” predictors may need to be identified and eliminated prior to modeling
+nzv <- nearZeroVar(normalEigenScaled, saveMetrics= TRUE)
+table(nzv$zeroVar)
+# FALSE 
+#398925 !!!!!!!!!!!!!!!!!!!
+#i=which(colnames(normalEigenScaled)==PAM50genes$ensembl_gene_id[PAM50genes$hgnc_symbol=="ESR1"])
+i=which(colnames(normalEigenScaled)==pam50$ensembl_gene_id[pam50$hgnc_symbol=="ESR1"])
+y=normalEigenScaled[,i] 
+normalEigenScaled=normalEigenScaled[,1:ncol(normalEigenScaled)!=i]
 
-which(rownames(expr)=="ESR1")
-#9
-train = sample (1:ncol(expr) ncol(x)/2)
+set.seed(123)
+train = sample (1:nrow(normalEigenScaled),2*nrow(normalEigenScaled)/3)
 test =(-train)
-predictors=t(do.call(rbind,concatenadas$normal[1:2]))
-grid =10^ seq (10 , -2 , length =100)
-#λ from 1010 to 10−2 covers the full scenarios from the null model containing only the intercept, to the least squares fit
-lasso.ESR1=glmnet(x=predictors[train,],
-#glmnet() function standardizes the variables so that they are on the same scale
-	y=expr[9,train],
+grid=seq(0,1,length.out=100)#should cover the full scenarios from the null model containing 
+#only the intercept, to the least squares fit
+cv.ESR1=cv.glmnet(x=as.matrix(normalEigenScaled[train,]),
+	y=y[train],
+	standardize=T,#data is alredy tranformed
 	alpha=0.5,
-	lambda=grid)#larger λ: smaller coefficient 
-
-cv.ESR1=cv.glmnet(x=predictors[train,],y=expr[9,train],alpha=0.5,nfolds=ncol(expr))
+	nfolds=5,
+	lambda=grid,#larger λ: smaller coefficient
+	type.gaussian = "naive")#p>500
+#glmnet chooses the lambda sequence such that the number of nonzero coefficients ranges from 0 
+#to p, where p is the total number of features
 plot(cv.ESR1)
+out = glmnet (x=as.matrix(normalEigenScaled),y=y,alpha=0.5,lambda=grid)
+coef.out = predict(out, type="coefficients", s=cv.ESR1$lambda.min) #min vs 1se
+#lambda.1se represents the λ that was simpler than the best model(lambda.min), but has error 
+#within 1 standard error of the best model, so it cannot be distinguished from the best model 
 mean((predict(lasso.ESR1,s=0,newx=predictors[test,])-expr[9,test]))
-#[1] -0.2096071
 predi.ESR1=predict(lasso.ESR1,s=cv.ESR1$lambda.min,newx=predictors[test,])
 mean((predi.ESR1-expr[9,test]))
-#[1] -0.06095145
-out = glmnet (x=predictors,y=expr[9,],alpha=0.5)
-coef.out = predict(out, type="coefficients", s=cv.ESR1$lambda.min)
 
-temp=coef(out)[,which(colSums(as.matrix(coef(out))>0)>1)]
-temp=temp[unique(which(as.matrix(temp)>0,arr.ind=T)[,1]),]
+#####################################################
+
+set.seed(123)
+model <- train(
+  y~., data = normalEigScldPCA, method = "glmnet",
+  trControl = trainControl("cv", number = 10),
+  tuneLength = 10
+)
+# Best tuning parameter
+model$bestTune
+coef(model$finalModel, model$bestTune$lambda)
