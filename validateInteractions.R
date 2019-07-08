734775ae92bc0acf63411be7ef11e4e42cab24b8
SantiagoDomiro <dopretocoracao@gmail.com>
2019-07-08 17:14:56 -0500

    added plots

--- validateInteractions.R
+++ validateInteractions.R
@@ -1,14 +1,22 @@
+lambda=unlist(lapply(quality,function(x) as.numeric(x[,3])))
+lambda=as.data.frame(cbind(lambda,as.character(sapply(names(quality),rep,50))))
+colnames(lambda)[2]="subtype"
+ggplot(lambda,aes(x=as.numeric(lambda)))+geom_density(aes(group=subtype,color=subtype,fill=subtype),alpha=0.3)+scale_x_continuous(minor_breaks=NULL)
+rmse=unlist(lapply(quality,function(x) as.numeric(x[,4])))
+rmse=as.data.frame(cbind(rmse,as.character(sapply(names(quality),rep,50))))
+colnames(rmse)[2]="subtype"
+ggplot(rmse,aes(x=as.numeric(rmse)))+geom_density(aes(group=subtype,color=subtype,fill=subtype),alpha=0.3)+scale_x_continuous(minor_breaks=NULL)
+
 library(biomaRt)
 library(rentrez)
 
-mart=useEnsembl("ensembl",dataset="hsapiens_gene_ensembl")
+mart=useEnsembl("ensembl",dataset="hsapiens_gene_ensembl",host="http://apr2019.archive.ensembl.org")
 myannot=getBM(attributes = c("ensembl_gene_id","hgnc_symbol","chromosome_name","start_position","end_position","external_gene_name","entrezgene"), mart=mart)
 myannot=myannot[!duplicated(myannot$ensembl_gene_id),]
 
 #hgnc_symbol instead of ensemblID
 interacs=unique(do.call(rbind,sifs))
 interacs=interacs[order(interacs[,1]),]
-interacs=interacs[interacs[,2]!="",]
 interacs=interacs[interacs[,2]!="(Intercept)",]
 targets=table(interacs[,1])
 temp=sapply(1:50,function(x) rep(myannot$hgnc_symbol[myannot$ensembl_gene_id==names(targets)[x]],targets[x]))
@@ -16,22 +24,10 @@ interacs=cbind(interacs,unlist(temp))
 interacs=interacs[order(interacs[,2]),]
 i=grep("ENSG",interacs[,2])
 interacs=cbind(interacs,interacs[,2])
-notarg=table(interacs[i,4])
+notarg=table(interacs[i,2])
 temp=sapply(1:length(notarg),function(x) rep(myannot$hgnc_symbol[myannot$ensembl_gene_id==names(notarg)[x]],notarg[x]))
-interacs[i,4]=unlist(temp)
+interacs[i,5]=unlist(temp)
 
-#how many times both entities are mentioned in the same paper
-comention=pbsapply(round(seq(1,21984,length=7)),function(i)
- apply(interacs[i:(i+3664),3:4],1,function(x) {
- 	reque=entrez_search(db = "pubmed", term = paste(x[1]," AND ",x[2],collapse=" "));
- 	Sys.sleep(0.1);
- 	return(reque)}))
-cuentas=as.numeric(apply(comention,c(1,2),function(x) unlist(x[[1]][2])))
-query=as.character(apply(comention,c(1,2),function(x) unlist(x[[1]][4])))
-comen=cbind(query,cuentas)
-comen=comen[comen[,2]!="0",]
-#cuentas=apply(interacs,1,function(x) comen[intersect(grep(x[3],comen[,1],ignore.case=T,fixed=T),grep(x[4],comen[,1],ignore.case=T)),2])
-#cat temp|perl -pe 'unless(/ AND \(/){s/.*?\sOR//g}unless(/\) AND \(/){s/\(.*?OR//g;s/\".*?OR //;s/(^ |\"|\)|\[All Fields\])//g}'
 ##############################################################################
 ########### miR
 ##############################################################################
@@ -41,10 +37,10 @@ library(multiMiR)#https://www.bioconductor.org/packages/devel/bioc/vignettes/mul
 #get all the interactions of selected miRs
 i=grep("hsa",interacs[,2])
 length(unique(interacs[i,2]))
-#[1] 241
-miRNAs   = list_multimir("mirna")
+#[1] 139
+miRNAs = list_multimir("mirna")
 sum(unique(interacs[i,2])%in%miRNAs$mature_mirna_id)
-#[1] 7
+#[1] 4
 mirInteractions=lapply(miRNAs$mature_mirna_id[miRNAs$mature_mirna_id%in%unique(interacs[i,2])],function(x)
 	get_multimir(mirna = x, summary = F,table="all")@data)
 #found interactions have been reported? NOP
@@ -86,13 +82,13 @@ if(target%in%Marbach2016[[TF]]){support=c(support,"Marbach2016")}
 return(paste(support,collapse=','))}#CAGE+binding motifs
 #no sé cuando recopilaron los datos
 
-intrcsTF=apply(interacs[i,3:4],1,function(x) knownTF(x[2],x[1]))
+intrcsTF=apply(interacs[i,4:5],1,function(x) knownTF(x[2],x[1]))
 sum(intrcsTF!="")
-#[1] 188 interactions with TFs reported in DB
+#[1] 202 interactions with TFs reported in DB
 interacs=cbind(interacs,NA)
-intercs[i,5]=intrcsTF
-colnames(interacs)=c("pam50","predictor","pam50Symbol","predictorSymbol","TFsupportedBy")
-length(unique(interacs[interacs[,5]!=""&!is.na(interacs[,5]),4]))
+interacs[i,6]=intrcsTF
+colnames(interacs)=c("pam50","predictor","coef","pam50Symbol","predictorSymbol","TFsupportedBy")
+length(unique(interacs[interacs[,6]!=""&!is.na(interacs[,6]),5]))
 #[1] 131 TFs with known TF-target interaction in tftargets
 
 ##############################################################################
@@ -103,23 +99,61 @@ methy=read.table("ini/hm450.hg38.manifest.tsv",sep='\t',header=T,fill=T)
 #am I linking genes with their known regulating CpGs? NOP
 i=grep("ENSG|hsa",interacs[,2],perl=T,invert=T)
 length(i)
-#[1] 4202
+#[1] 3
 knownReg=sapply(i,function(x) 
-	grep(interacs[x,3],methy$gene_HGNC[methy$probeID==interacs[x,4]]))
+	grep(interacs[x,4],methy$gene_HGNC[methy$probeID==interacs[x,2]]))
 sum(sapply(knownReg,length)==0)
-#[1] 4202
+#[1] 3
 #are they in the same chr?
 sameChr=apply(interacs[i,1:2],1,function(x) 
 	sum(myannot$chromosome_name[myannot$ensembl_gene_id==x[1]]==methy$CpG_chrm[methy$probeID==x[2]]))
 sum(sameChr!=0) 
-#[1] 173 sólo estos están en el mismo cromosoma y están lejos ↓
-interacs=cbind(interacs,NA)
-interacs[i,6]="n"
-interacs[i[sameChr!=0],6]="y"
-temp=interacs[which(interacs[,6]=="y"),]#todos afectan PTTG1  
+#[1] 1 sólo este está en el mismo cromosoma y están lejos ↓
+temp=interacs[i[sameChr!=0],]
+#          pam50  predictor       coef pam50Symbol predictorSymbol
+#ENSG00000129514 cg00955911 -0.2298019        ANLN      cg00955911
 apply(methy[methy$probeID%in%temp[,2],2:3]-myannot$start_position[myannot$ensembl_gene_id=="ENSG00000164611"],2,function(x) min(abs(x)))
 #CpG_beg CpG_end 
-#2846047 2846045 
+#122829073 122829071 
+     
+#####################################################################
+#######how many times terms are mentioned in literature
+#####################################################################
+#both on the same paper
+comention=pbsapply(round(seq(1,11119,length=20)),function(i)
+  apply(interacs[i:(i+585),4:5],1,function(x) {
+  reque=entrez_search(db = "pubmed", term = paste(x[1]," AND ",x[2],collapse=" "));
+  Sys.sleep(0.1);
+ return(reque)}))
+
+cuentas=as.numeric(apply(comention,c(1,2),function(x) unlist(x[[1]][2])))
+query=as.character(apply(comention,c(1,2),function(x) unlist(x[[1]][4])))
+comen=cbind(query,cuentas)
+comen=comen[comen[,2]!="0",]
+#cat temp|perl -pe 'unless(/ AND \(/){s/.*?\sOR//g}unless(/\) AND \(/){s/\(.*?OR//g;s/\".*?OR //;s/(^ |\"|\)|\[All Fields\])//g}'
+comention=read.table("Downloads/temp",sep='\t',header=T)
+temp=t(sapply(strsplit(as.character(comention$query)," +AND +",perl=T),as.character))
+comention=cbind(comention,temp)
+comention[,3]=toupper(comention[,3])
+comention[,4]=toupper(comention[,4])
+
+#each mentioned
+query=unique(unlist(comention[,3:4]))
+mention=pbsapply(query,function(x){
+	reque=entrez_search(db = "pubmed", term = x);
+	Sys.sleep(0.1);
+	return(reque)})
+mention=as.matrix(t(mention)[,2])
+
+i=apply(comention,1,function(x) which(interacs[,3]==x[3]&interacs[,4]==x[4]))
+interacs=cbind(interacs,NA)
+interacs[i,7]=comention$cuentas
+temp=t(apply(interacs[i,],1,function(x) 
+	cbind(mention[rownames(mention)==x[3],2],mention[rownames(mention)==x[4],2])))
+interacs=cbind(interacs,NA,NA)
+interacs[i,8]=temp[,1]
+interacs[i,9]=temp[,2]
+colnames(interacs)[7:9]=c("comention","pam50Mention","predictorMention")
 
 #####################################################################
 ######all together per subtype
@@ -129,49 +163,6 @@ sifs1=lapply(sifs,function(x) x[x[,2]!="(Intercept)",])
 temp=lapply(sifs,function(x) do.call(rbind,apply(x,1,function(y) interacs[interacs[,1]==y[1]&interacs[,2]==y[2],])))
 sifs1=lapply(1:5,function(x) cbind(as.matrix(sifs1[[x]][,3]),as.matrix(temp[[x]])))
 names(sifs1)=names(sifs)
-lapply(sifs1,function(x) apply(x[,6:7],2,table,useNA="ifany"))
-#$Basal$TFsupportedBy
-#                                 ITFP        Marbach2016             TRRUST 
-#              4361                  4                 18                  1 
-#TRRUST,Marbach2016               <NA> 
-#                 2                982 
-#$Basal$sameChr
-#   n    y <NA> 
-# 852   54 4462 
-#$Her2$TFsupportedBy
-#                   ITFP Marbach2016        <NA> 
-#       1945           3           7        2343 
-#$Her2$sameChr
-#   n    y <NA> 
-#2237   72 1989 
-#$LumB$TFsupportedBy
-#                 ENCODE        ITFP Marbach2016        TRED TRED,TRRUST 
-#       3500           1           3          16           1           1 
-#     TRRUST        <NA> 
-#          1         837 
-#$LumB$sameChr
-#   n    y <NA> 
-# 752   30 3578 
-#$normal$TFsupportedBy
-#                                           ITFP        ITFP,Marbach2016 
-#                   2682                      50                       1 
-#ITFP,TRRUST,Marbach2016             Marbach2016             TRED,TRRUST 
-#                      1                      11                       1 
-#     TRRUST,Marbach2016                    <NA> 
-#                      2                      78 
-#$normal$sameChr
-#<NA> 
-#2826 
-#$LumA$TFsupportedBy
-#                                         ENCODE                    ITFP 
-#                   8335                       1                      14 
-#           Marbach2016 TRED,TRRUST,Marbach2016      TRRUST,Marbach2016 
-#                     47                       1                       1 
-#                   <NA> 
-#                    397 
-#$LumA$sameChr
-#   n    y <NA> 
-# 188   17 8591 
 sifs1=lapply(sifs1,function(x) x[,c(2:3,1,4:7)])
 #####################################################################
 ######coherence of regulation
@@ -194,3 +185,10 @@ colnames(sifs1[[3]])[c(3,8:9)]=c("beta","pam50UP","predictorUP")
 colnames(sifs1[[4]])[c(3,8:9)]=c("beta","pam50UP","predictorUP")
 colnames(sifs1[[5]])[3]="beta"
 save(sifs1,file="annotatedSifsAlpha0.5.RData")
+#####################################
+i=unique(unlist(lapply(sifs,function(x) unique(x[,2]))))
+i=which(rownames(concatenadas$LumA)%in%i)
+selec=do.call(cbind,lapply(concatenadas,function(x) x[i,]))
+pdf("selectedHeat.pdf")
+heatmap.2(selec,col=rev(heat.colors(74)),scale="none",Colv=F,trace="none",ColSideColors=rainbow(5)[subtis],symm=F,symkey=F,labCol=NA,dendrogram="row",breaks=col,labRow=NA,RowSideColors=c("cornflowerblue","brown1")[rowColors])
+dev.off()
