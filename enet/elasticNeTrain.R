cf1acc6ba123325982ee339348f1e71d7e363131
SantiagoDomiro <dopretocoracao@gmail.com>
2019-10-29 15:50:04 -0600

    corrected rownames of output

--- enet/elasticNeTrain.R
+++ enet/elasticNeTrain.R
@@ -16,9 +16,11 @@ subtipo=fread(paste("../",subtipo,sep=""),sep='\t')
 nombres=subtipo$V1
 subtipo$V1=NULL
 i=round(ncol(subtipo)*0.8)
-subtipo=t(as.matrix(subtipo)[,1:i])#training subset should not be used for testing
+subtipo=t(as.matrix(subtipo))
 colnames(subtipo)=nombres
-#rm(EigenScaled);gc()
+training=subtipo[1:i,]
+testing=subtipo[(i+1):nrow(subtipo),]
+
 
 coefGrid <-  expand.grid(lambda=10^ seq (3 , -2 , length =50),#length is usually 100
 			alpha=0.5)
@@ -41,9 +43,10 @@ model <- train(y = training[,colnames(training)==gen],
 	       intercept=F)
 ajuste=RMSE(predict(model,testing),testing[,colnames(testing)==gen])
 coefs=as.matrix(coef(model$finalModel, model$bestTune$lambda))
-coefs=as.matrix(coefs[which(coefs!=0),])
-colnames(coefs)=paste("lamda",model$bestTune$lambda,"RMSE",ajuste,sep=':')
-write.table(coefs,
+coefs1=as.matrix(coefs[which(coefs!=0),])
+rownames(coefs1)=rownames(coefs)[coefs!=0]
+colnames(coefs1)=paste("lambda",model$bestTune$lambda,"RMSE",ajuste,sep=':')
+write.table(coefs1,
 	    file=paste(gen,args[1],"coefs",sep='.'),
 	    quote=F,
 	    sep='\t')
