fb72051729de4b50cc5b88d8a06f78e856a21d36
SantiagoDomiro <dopretocoracao@gmail.com>
2020-08-01 19:17:58 -0500

    summary of potential regulators

--- MInet/potentialRegus.R
+++ MInet/potentialRegus.R
@@ -41,45 +41,90 @@ regus=lapply(regus,function(x)
 	lapply(x,function(y) 
 		y[c(which(substr(y,1,1)%in%c('c','h')),
 			which(y%in%myannot$ensembl_gene_id[myannot$hgnc_symbol%in%tfs$V3]))]))
+names(regus)=names(top)
+#not all BPs have potential regulators
 sapply(regus,function(x) sum(sapply(x,length)==0))
 # Basal   Her2   LumA   LumB normal 
 #    16     18      4     11     31 
-#not all BPs have potential regulators
+#not interested in BPs enriched only in the net from normal tissue
+regus$normal=regus$normal[names(regus$normal)%in%unlist(sapply(regus[1:4],names))]
 
-##################how many regus per bp??????????????
-
-#data frame for barplot
-names(regus)=names(top)
-temp=lapply(regus,function(x) lapply(x,function(y) table(substr(y,1,1))))
-temp=lapply(temp,function(x) lapply(x,function(y) cbind(names(y),y)))
-temp=lapply(temp,function(x) do.call(rbind,lapply(1:length(x),function(y) 
-	cbind(names(x)[y],x[[y]]))))
-temp=data.frame(do.call(rbind,lapply(1:5,function(x) 
-	cbind(names(temp)[x],temp[[x]]))
-colnames(temp)=c("subtype","bp","omic","sum")
+#regulator's combination per subtype
+temp=lapply(regus,function(x) table(unlist(lapply(x,function(y) 
+	paste(unique(substr(y,1,1)),collapse="")))))
+temp=lapply(temp,function(x) cbind(names(x),x))
+temp=do.call(rbind,lapply(1:5,function(x) cbind(names(temp)[x],temp[[x]])))
+temp[temp[,2]=="",2]="no regulator"
+temp=data.frame(temp)
+colnames(temp)=c("subtype","combo","sum")
 temp$sum=as.numeric(as.character(temp$sum))
+#make omic ids explicit
+temp$combo=gsub("c","CpG+",temp$combo)
+temp$combo=gsub("h","miRNA+",temp$combo)
+temp$combo=gsub("E","TF",temp$combo)
+temp$combo[temp$combo=="CpG+"]="CpG"
+temp$combo[temp$combo=="miRNA+"]="miRNA"
+temp$combo[temp$combo=="miRNA+CpG+"]="miRNA+CpG"
+temp$combo=factor(temp$combo,levels=c("CpG","TF","miRNA","CpG+TF","miRNA+CpG","miRNA+TF","miRNA+CpG+TF","no regulator"))
 
-ggplot(temp,aes(x=subtype,y=x,fill=regulator))+
+png("regulatorsCombo.png")
+ggplot(temp,aes(x=subtype,y=sum,fill=combo))+
 geom_bar(position="stack", stat="identity")+
 ylab("biological processes")+xlab("")+
 theme(text=element_text(size=18))+
-scale_fill_manual(values=c("skyblue4","skyblue3","skyblue2","skyblue1","plum3","plum4"))
+scale_fill_manual("regulators",values=c("#62929E","#55DBCB","#F9DF74","#39A2AE","gold3","#75E4B3","#A8AA74","ivory3"))
+dev.off()
 #colors have to be adjusted
-#for heatmap
+
+#% of each potential regulator per BP
 temp=lapply(regus,function(x) lapply(x,function(y) table(substr(y,1,1))))
 temp=lapply(temp,function(x) lapply(x,function(y) cbind(names(y),y)))
+temp=lapply(temp,function(x) x[sapply(x,length)>0])
 temp=lapply(temp,function(x) do.call(rbind,lapply(1:length(x),function(y) 
 	cbind(names(x)[y],x[[y]]))))
 temp=data.frame(do.call(rbind,lapply(1:5,function(x) 
-	cbind(names(temp)[x],temp[[x]]))
+	cbind(names(temp)[x],temp[[x]]))))
 colnames(temp)=c("subtype","bp","omic","sum")
 temp$sum=as.numeric(as.character(temp$sum))
+temp$subtype=paste(temp$subtype,temp$omic)
+#matrix of subtype-regulator type vs BP
 M=graph.data.frame(temp[,1:2],directed=F)
 E(M)$weight=temp$sum
 M=as.matrix(M[unique(temp[,2]),unique(temp[,1])])
 M[M==0]=NA
 M=lapply(names(top),function(x) M[,grep(x,colnames(M))])
-total=sapply(M,rowSums)
+total=sapply(M,rowSums,na.rm=T)
+#get percentages
 M=do.call(cbind,lapply(1:5,function(x) 100*M[[x]]/total[,x]))
-#u still have get % of to plot
+#order by % of TFs in normal tissue BPs
+M=M[order(M[,14],decreasing=T),]
+
+pdf("RegusTot.pdf",height=16)
+gplots::heatmap.2(M,Colv=F,Rowv=F,dendrogram='n',scale='n',trace='n',
+	col=rev(heat.colors(100))[5:100],labCol=rep(c("CpG","TF","miRNA"),5),
+	lmat = rbind(c(0,3),c(2,1),c(0,4)),lhei=c(0.2,7,2),lwid=c(0.2,5),key.title="",
+	margins=c(2,25),add.expr=text(x=c(2,5,8,11,14),y=168,xpd=NA,
+	labels=names(top)),key.xlab="%")
+#xy positions need to be adjusted manually
+dev.off()
+
+#regulators per BP
+temp=lapply(regus,function(x) sapply(x,function(y) substr(y,1,1)))
+temp1=unique(unlist(temp$LumA))
+temp=lapply(temp,function(z) do.call(rbind,lapply(temp1,function(x)
+ do.call(rbind,lapply(z,function(y) cbind(x,sum(y==x)))))))
+temp=data.frame(do.call(rbind,lapply(1:5,function(x) cbind(names(temp)[x],temp[[x]]))))
+colnames(temp)=c("subtype","omic","sum")
+temp$sum=as.numeric(as.character(temp$sum))
+temp$omic=gsub("c","CpG",temp$omic)
+temp$omic=gsub("h","miRNA",temp$omic)
+temp$omic=gsub("E","TF",temp$omic)
+
+png("perBPdistris.png")
+ggplot(temp,aes(y=sum,x=subtype,color=subtype))+ylim(0,25)+geom_boxplot()+
+facet_wrap(~omic)+ylab("regulators per process")+
+theme(text=element_text(size=18),axis.text.x=element_blank())+xlab("")
+dev.off()
+
+
 mir=fread("miR.ids.map.tsv",skip=1)
