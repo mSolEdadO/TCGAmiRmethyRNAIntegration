c82d75f6e583244a4f03a2a87342f986bd7806d3
SantiagoDomiro <dopretocoracao@gmail.com>
2020-10-09 20:26:50 -0500

    updated plots

--- MInet/3.potentialRegus.R
+++ MInet/3.potentialRegus.R
@@ -2,6 +2,7 @@ library(igraph)
 library(data.table)
 library(tidyverse)
 library(ggplot2)
+library(gridExtra)
 #sifs as graphs so nodes can easily be extracted
 g=lapply(BPenriched,function(x) graph.data.frame(x[,c(1,3)],directed=F))
 #########################POTENTIAL REGULATORS PER SUBTYPE#########################
@@ -77,17 +78,32 @@ temp=lapply(temp,function(x) do.call(rbind,lapply(1:length(x),function(y)
 	cbind(names(x)[y],x[[y]]))))
 temp=data.frame(do.call(rbind,lapply(1:5,function(x) 
 	cbind(names(temp)[x],temp[[x]]))))
-colnames(temp)=c("subtype","bp","omic","sum")
+colnames(temp)=c("subtype","bp","regulator","sum")
 temp$sum=as.numeric(as.character(temp$sum))
-temp$subtype=paste(temp$subtype,temp$omic)
+temp$regulator=gsub("E","TF",gsub("h","miRNA",gsub("c","CpG",temp$regulator)))
+temp$regulator=factor(temp$regulator,levels=c("CpG","TF","miRNA"))
+
+#area plots
+plot1=ggplot(temp[temp$subtype!="normal",],aes(y=sum,x=as.numeric(bp)))+
+ geom_area(aes(fill=regulator),position="fill")+facet_wrap(~subtype)+ylab("proportion")+
+ xlab("biological processes")+theme(text=element_text(size=18),axis.ticks.x=element_blank(),
+ 	axis.text.x=element_blank(),legend.position='n')+scale_fill_manual(values=gray.colors(5))
+plot2=ggplot(temp[temp$subtype=="normal",],aes(y=sum,x=as.numeric(bp)))+
+ geom_area(aes(fill=regulator),position="fill")+ylab("proportion")+xlab("biological processes")+
+ theme(text=element_text(size=18),axis.ticks.x=element_blank(),
+ 	axis.text.x=element_blank())+scale_fill_manual(values=gray.colors(5))+ggtitle("normal")
+png("RegusPropor.png",width=800)
+grid.arrange(plot1,plot2,ncol=2)
+dev.off()
+
+temp$subtype=paste(temp$subtype,temp$regulator)
 #matrix of subtype-regulator type vs BP
 M=graph.data.frame(temp[,1:2],directed=F)
 E(M)$weight=temp$sum
 M=as.matrix(M[unique(temp[,2]),unique(temp[,1])])
 M=M[,c(1:12,15,13,14)]#so the order is the same for all subtypes
+#separate per subtype
 M=lapply(names(top),function(x) M[,grep(x,colnames(M))])
-total=sapply(M,rowSums,na.rm=T)
-#get percentages
 
 #test independence of regulators vs subtype
 c_pvals=lapply(1:4,function(y) p.adjust(sapply(1:176,function(x) 
@@ -107,56 +123,39 @@ sapply(1:4,function(x) sum(t_pvals[[x]][intersect(i[[x]],i[[5]])]<0.05)/length(i
 sapply(1:4,function(x) sum(m_pvals[[x]][intersect(i[[x]],i[[5]])]<0.05)/length(intersect(i[[x]],i[[5]])))
 #[1] 0.3214286 0.7111111 0.3103448 0.6344086
 
-
+total=sapply(M,rowSums,na.rm=T)
+#get percentages
 M=do.call(cbind,lapply(1:5,function(x) 100*M[[x]]/total[,x]))
 #order columns
 M[is.na(M)]=0
 
-pdf("RegusTot.pdf",height=18)
+pdf("RegusTot.pdf",height=15)
 gplots::heatmap.2(M,Colv=F,dendrogram='r',scale='n',sepcolor="gray",
 	col=rev(heat.colors(100)),trace='n',labCol=rep(c("CpG","TF","miRNA"),5),
-	lmat = rbind(c(0,3),c(2,1),c(0,4)),lhei=c(0.2,5,2),lwid=c(1,6),
+	lmat = rbind(c(0,3),c(2,1),c(0,4)),lhei=c(0.2,5,.5),lwid=c(1,6),
 	key.title="",margins=c(2,25),density.info="n",
-	add.expr=text(x=c(2,5,8,11,14),y=178,xpd=NA,labels=names(top)),key.xlab="%")
+	add.expr=text(x=c(2,5,8,11,14),y=178,xpd=NA,labels=names(top)),key.xlab="%",keysize=.25)
 #xy positions need to be adjusted manually
 dev.off()
 
 #regulators per BP
-temp$subtype=sapply(strsplit(temp$subtype," "),function(x) x[1])
-temp$omic=gsub("c","CpG",temp$omic)
-temp$omic=gsub("h","miRNA",temp$omic)
-temp$omic=gsub("E","TF",temp$omic)
-temp$omic=factor(temp$omic,levels=c("CpG","TF","miRNA"))
+#temp$subtype=sapply(strsplit(temp$subtype," "),function(x) x[1])
+#temp$omic=gsub("c","CpG",temp$regulator)
+#temp$omic=gsub("h","miRNA",temp$regulator)
+#temp$omic=gsub("E","TF",temp$regulator)
+#temp$omic=factor(temp$omic,levels=c("CpG","TF","miRNA"))
 
-png("perBPdistris.png")
- ggplot(temp,aes(y=sum,x=subtype,color=subtype))+ylim(0,900)+geom_boxplot()+
- facet_wrap(~omic)+ylab("regulators per process")+
- theme(text=element_text(size=18),axis.text.x=element_blank())+xlab("")
-dev.off()
+#png("perBPdistris.png")
+# ggplot(temp,aes(y=sum,x=subtype,color=subtype))+ylim(0,900)+geom_boxplot()+
+# facet_wrap(~omic)+ylab("regulators per process")+
+# theme(text=element_text(size=18),axis.text.x=element_blank())+xlab("")
+#dev.off()
 #Removed 1 rows containing non-finite values (stat_boxplot). 
 
 #test distributions
-temp=lapply(unique(temp$omic),function(x) temp[temp$omic==x,])
-names(temp)=sapply(temp,function(x) x$omic[1])
-lapply(temp,function(z) 
-	matrix(round(p.adjust(sapply(names(top),function(x) sapply(names(top),
-		function(y) ks.test(z$sum[z$subtype==x],z$sum[z$subtype==y])$p.val)),
-					"fdr"),4),ncol=5))
-
-#total of regulators per subtype adding all BPs
-temp=do.call(rbind,temp)
-temp1=sapply(unique(temp$subtype),function(x) sapply(levels(temp$omic),function(y)
-	sum(temp$sum[temp$subtype==x&temp$omic==y])))
-#data frama to plot
-temp=data.frame(cbind(rownames(temp1),temp1))
-temp=temp%>%pivot_longer(-V1,names_to="subtype",values_to="proportion")
-temp$sum=as.numeric(as.character(temp$proportion))
-colnames(temp)[1]="regulator"
-temp$regulator=factor(temp$regulator,levels=c("CpG","TF","miRNA"))
-
-png("RegusEnrich.png")
- ggplot(temp,aes(y=proportion,x=subtype,fill=regulator))+
- geom_bar(position="fill", stat="identity")+
- theme(text=element_text(size=18))+xlab("")+
- scale_fill_manual(values=gray.colors(5))
-dev.off()
+#temp=lapply(unique(temp$omic),function(x) temp[temp$omic==x,])
+#names(temp)=sapply(temp,function(x) x$omic[1])
+#lapply(temp,function(z) 
+#	matrix(round(p.adjust(sapply(names(top),function(x) sapply(names(top),
+#		function(y) ks.test(z$sum[z$subtype==x],z$sum[z$subtype==y])$p.val)),
+#					"fdr"),4),ncol=5))
