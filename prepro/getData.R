4ed040f0482b779dc7836046a1f468bae157850e
SantiagoDomiro <dopretocoracao@gmail.com>
2020-12-08 14:25:45 -0600

    corrected barcodes

--- prepro/getData.R
+++ prepro/getData.R
@@ -6,56 +6,56 @@ library(VennDiagram)
 mthyltn <-  GDCquery(project = "TCGA-BRCA",
 	data.category = "DNA Methylation",
 	platform="Illumina Human Methylation 450")
-j=substr(getResults(mthyltn)$cases,1,12)
+mthyltn=getResults(mthyltn)
+i=substr(mthyltn$cases,1,19)
 xprssn <- GDCquery(project = "TCGA-BRCA",
   data.category = "Transcriptome Profiling",
   data.type = "Gene Expression Quantification",
   workflow.type = "HTSeq - Counts")
-i=substr(getResults(xprssn)$cases,1,12)
+xprssn=getResults(xprssn)
+j=substr(xprssn$cases,1,19)
 mirnas <- GDCquery(project = "TCGA-BRCA",
 	data.category = "Transcriptome Profiling",
 	data.type = "miRNA Expression Quantification")
-k=substr(getResults(mirnas)$cases,1,12)
+mirnas=getResults(mirnas)
+k=substr(mirnas$cases,1,19)
 
-sapply(list(j,i,k),function(x) length(unique(x)))
-#[1]  789 1092 1079
-#matching ids
-patients=intersect(j,intersect(i,k))
-length(patients)
-#[1] 777
+sapply(list(i,j,k),function(x) length(unique(x)))
+#[1]  890 1217 1202
+#only samples with concurrent measurements are useful
+samples=intersect(intersect(i,j),k)
+length(samples)
+#[1] 848
 
 #sample subtypes
-subtype=TCGA_MolecularSubtype(
-	getResults(xprssn)$cases)$subtypes
-sum(!patients%in%subtype$patients)#non classified samples
-#[1] 5
+subtype=TCGA_MolecularSubtype(xprssn$cases)$subtypes
+subtype$barcode=substr(subtype$samples,1,19)
+sum(!samples%in%subtype$barcode)#non classified samples
+#[1] 6
+#only classified samples  are useful
+samples=samples[samples%in%subtype$barcode]
+table(unique(subtype[subtype$barcode%in%samples,c(1,2)])[,2])#do not count duplicates
+# BRCA.Basal   BRCA.Her2   BRCA.LumA   BRCA.LumB BRCA.Normal 
+#        126          46         423         146         101 
+
 #plot intersections
 lapply(unique(subtype$subtype),function(x)
-	venn.diagram(x=list(A=unique(i[i%in%subtype$patients[subtype$subtype==x]]),
-		B=unique(j[j%in%subtype$patients[subtype$subtype==x]]),
-		C=unique(k[k%in%subtype$patients[subtype$subtype==x]])),
+	venn.diagram(x=list(
+		A=unique(j[j%in%subtype$barcode[subtype$subtype==x]]),
+		B=unique(i[i%in%subtype$barcode[subtype$subtype==x]]),
+		C=unique(k[k%in%subtype$barcode[subtype$subtype==x]])),
 		col = "transparent", fill = c("cornflowerblue","green","red"),
 		alpha = 0.50,cex = 1.5,cat.cex = 1.5,margin = 0.1,
 		fontfamily = "sans",cat.fontfamily=rep("sans",3),
 		category.names=c("RNAseq","HM450","miRNAseq"),filename=x))
 
-#get data of classified samples only 
-patients=patients[patients%in%subtype$patients]
-table(unique(subtype[,c(2,4)])[,1])#do not count duplications
-# BRCA.Basal   BRCA.Her2   BRCA.LumA   BRCA.LumB BRCA.Normal 
-#        127          46         420         146         112 
+subtype=subtype[subtype$barcode%in%samples,]
+write.table(subtype,"subtype.tsv",sep='\t',quote=F,row.names=F)
 
-#get ALL sample ids for these patients
-mthyltn <-  GDCquery(project = "TCGA-BRCA",
-	data.category = "DNA Methylation",
-	platform="Illumina Human Methylation 450",
-	barcode=patients)
-xprssn <- GDCquery(project = "TCGA-BRCA",
-  data.category = "Transcriptome Profiling",
-  data.type = "Gene Expression Quantification",
-  workflow.type = "HTSeq - Counts",
-  barcode=patients)
-mirnas <- GDCquery(project = "TCGA-BRCA",
-	data.category = "Transcriptome Profiling",
-	data.type = "miRNA Expression Quantification",
-	barcode=patients)
+#some samples were measured twice
+sum(duplicated(i[i%in%samples]))
+#[1] 2
+sum(duplicated(j[j%in%samples]))
+#[1] 4
+sum(duplicated(k[k%in%samples]))
+#[1] 4
