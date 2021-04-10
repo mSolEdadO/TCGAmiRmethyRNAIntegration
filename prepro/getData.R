1df270fe5348d33f04566bcc0aaf83cee4d3f157
SantiagoDomiro <dopretocoracao@gmail.com>
2021-04-10 17:01:12 -0500

    TCGAbiolinks update solved subtypes issue

--- prepro/getData.R
+++ prepro/getData.R
@@ -2,24 +2,25 @@ library(SummarizedExperiment)
 library(TCGAbiolinks)
 library(VennDiagram)
 
-#get sample ids per data type
+##########SAMPLE IDs PER DATA TYPE#####################
 mthyltn <-  GDCquery(project = "TCGA-BRCA",
 	data.category = "DNA Methylation",
 	platform="Illumina Human Methylation 450")
 mthyltn=getResults(mthyltn)
-i=substr(mthyltn$cases,1,15)
+i=substr(mthyltn$cases,1,19)
 xprssn <- GDCquery(project = "TCGA-BRCA",
   data.category = "Transcriptome Profiling",
   data.type = "Gene Expression Quantification",
   workflow.type = "HTSeq - Counts")
 xprssn=getResults(xprssn)
-j=substr(xprssn$cases,1,15)
+j=substr(xprssn$cases,1,19)
 mirnas <- GDCquery(project = "TCGA-BRCA",
 	data.category = "Transcriptome Profiling",
 	data.type = "miRNA Expression Quantification")
 mirnas=getResults(mirnas)
-k=substr(mirnas$cases,1,15)
+k=substr(mirnas$cases,1,19)
 
+##############CONCOURRENT MEASURES########################
 sapply(list(i,j,k),function(x) length(unique(x)))
 #[1]  890 1217 1202
 #only samples with concurrent measurements are useful
@@ -27,49 +28,66 @@ samples=intersect(intersect(i,j),k)
 length(samples)
 #[1] 848
 
+##############SUBTYPE PER SAMPLE########################
+#tissue per sample
+samples=data.frame(cbind(samples,
+	sapply(samples,function(x) 
+		unique(as.character(mthyltn$tissue.definition[i==x])))))
+colnames(samples)[2]="tissue"
+samples$patient=substr(samples$samples,1,12)
+
 #download subtypes
-subtype=TCGA_MolecularSubtype(xprssn$cases)$subtypes
-#https://www.cbioportal.org/study/clinicalData?id=brca_tcga_pan_can_atlas_2018
-subtype$barcode=substr(subtype$samples,1,15)
-sum(!samples%in%subtype$barcode)#non classified samples
-#[1] 6
+patients=TCGAquery_subtype(tumor="brca")#subtype per patient
+sum(!substr(samples,1,12)%in%patients$patient)
+#[1] 9
 #only classified samples  are useful
-samples=samples[samples%in%subtype$barcode]
-table(unique(subtype[subtype$barcode%in%samples,c(1,2)])[,2])#do not count duplicates
-# BRCA.Basal   BRCA.Her2   BRCA.LumA   BRCA.LumB BRCA.Normal 
-#        126          46         423         146         101 
+samples=samples[samples$patient%in%subtypes$patient,]
+table(subtypes$BRCA_Subtype_PAM50[subtypes$patient%in%samples$patient])
+#Basal   Her2   LumA   LumB Normal 
+#   129     46    416    140     34 
+#normal subtype is discarded NOT normal tissue
+samples=samples[which(!samples$patient%in%subtypes$patient[
+	subtypes$BRCA_Subtype_PAM50=="Normal"]|
+	samples$tissue=="Solid Tissue Normal"),]
+
+#71 patients are duplicated in DB
+temp=table(samples[samples$patient%in%samples$patient[duplicated(
+	samples$patient)],2:3])
+table(apply(temp,2,paste,collapse=""))
+#65 patients have 1 tumor & 1 normal samples
+#2 patients have 2 tumor samples
+#2 patients have 1 tumor & 1 metastatic samples
+#2 patients have 1 of each
+
+#subtype per sample
+samples$subtype=sapply(samples$patient,function(x) 
+	subtypes$BRCA_Subtype_PAM50[subtypes$patient==x])
+samples$subtype[samples$tissue=="Solid Tissue Normal"]="Normal"
+table(samples$subtype)
+# Basal   Her2   LumA   LumB Normal 
+#   128     46    416    140     75 
+#TCGA-A7-A0CE is missing from Basal coz there's no methylation data
+write.table(samples,"subtype.tsv",sep='\t',quote=F,row.names=F)
 
 #plot intersections
-lapply(unique(subtype$subtype),function(x)
+i=i[mthyltn$tissue.definition!="Solid Tissue Normal"]
+j=j[xprssn$tissue.definition!="Solid Tissue Normal"]
+k=k[mirnas$tissue.definition!="Solid Tissue Normal"]
+lapply(unique(samples$subtype),function(x)
 	venn.diagram(x=list(
-		A=unique(j[j%in%subtype$barcode[subtype$subtype==x]]),
-		B=unique(i[i%in%subtype$barcode[subtype$subtype==x]]),
-		C=unique(k[k%in%subtype$barcode[subtype$subtype==x]])),
+		A=unique(j[substr(j,1,12)%in%subtypes$patient[subtypes$BRCA_Subtype_PAM50==x]]),
+		B=unique(i[substr(i,1,12)%in%subtypes$patient[subtypes$BRCA_Subtype_PAM50==x]]),
+		C=unique(k[substr(k,1,12)%in%subtypes$patient[subtypes$BRCA_Subtype_PAM50==x]])),
 		col = "transparent", fill = c("cornflowerblue","green","red"),
 		alpha = 0.50,cex = 1.5,cat.cex = 1.5,margin = 0.1,
 		fontfamily = "sans",cat.fontfamily=rep("sans",3),
 		category.names=c("RNAseq","HM450","miRNAseq"),filename=x))
 
-subtype=subtype[subtype$barcode%in%samples,]
-write.table(subtype,"subtype.tsv",sep='\t',quote=F,row.names=F)
-
-#some patients were measured several times
-sum(duplicated(subtype$patients))
-#[1] 71
-#12 are NOT normal-tumor pairs
-temp=table(subtype[subtype$patients%in%
-	subtype$patients[duplicated(subtype$patients)],2:3])
-sum(temp[5,]!=1)
-#[1] 12
-#TCGA-E9-A1NF has 1 LumA sample & 1 LumB
-#TCGA-BH-A1FE has 2 normal samples + 1 LumA
-
-#can duplicates be explained by metastasis?
-#sum(subtype$samples%in%xprssn$cases)
-tissue=xprssn[xprssn$cases%in%subtype$samples,c(4,28)]
-tissue=tissue[order(match(tissue$cases,subtype$samples)),]
-subtype=cbind(subtype,tissue$tissue.definition)
-colnames(subtype)[5]="tissue"
-#problems with sample IDs
-lapply(unique(subtype$tissue),function(x) 
-	table(subtype[subtype$tissue==x,c(2,5)]))
+venn.diagram(x=list(
+	A=substr(mthyltn$cases[mthyltn$tissue.definition=="Solid Tissue Normal"],1,19),
+	B=substr(xprssn$cases[xprssn$tissue.definition=="Solid Tissue Normal"],1,19),
+	C=substr(mirnas$cases[mirnas$tissue.definition=="Solid Tissue Normal"],1,19)),
+	col = "transparent", fill = c("cornflowerblue","green","red"),
+	alpha = 0.50,cex = 1.5,cat.cex = 1.5,margin = 0.1,
+	fontfamily = "sans",cat.fontfamily=rep("sans",3),
+	category.names=c("RNAseq","HM450","miRNAseq"),filename="Normal")
