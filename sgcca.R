54e468bf6c6bfa5ba88fb151cf4fc2049655d60b
SantiagoDomiro <dopretocoracao@gmail.com>
2021-06-25 20:54:08 -0500

    fitting sparsity

--- sgcca.R
+++ sgcca.R
@@ -22,16 +22,30 @@ names(normi)=c("CpGs","transcripts","miRNAs")
 #using an internal 5-fold CV loop: the parameters that minimize the
 #prediction error [Tenenhaus2014]
 
-params_searcher=function(subtype,lCpG,ltranscri,lmir){
+params_searcher=function(subtype,lCpG,ltrnscri,lmir){
 	size=nrow(subtype$miRNAs)
 	i=sample(1:size,round(size/2))
 	data=lapply(subtype,function(y) y[i,])
-	results=wrapper.sgcca(data,penalty=c(lCpG,ltranscri,lmir),scale=T)#ncomp=1 for training
-	evar=as.data.frame(do.call(cbind,results$explained_variance))
-	evar$component=gsub("comp ","",rownames(evar))
-	evar=evar%>%pivot_longer(-component,names_to="omic",values_to="explained_variance")
-	features=as.data.frame(do.call(cbind,lapply(results$loadings,function(x) apply(x,2,function(y) sum(y!=0)))))
-	features$component=gsub("comp","",rownames(features))
-	features=features%>%pivot_longer(-component,names_to="omic",values_to="features")
-	return(merge(evar,features,by=c("component","omic")))}
-lapply(1:5,function(x) params_searcher())
+	results=wrapper.sgcca(data,penalty=c(lCpG,ltrnscri,lmir),scale=T)
+	#ncomp=1 for training
+	evar=as.data.frame(do.call(rbind,results$explained_variance))
+	features=as.data.frame(do.call(rbind,lapply(results$loadings,
+		function(x) apply(x,2,function(y) sum(y!=0)))))
+	out=cbind(evar,features,results$penalty)
+	out$omic=rownames(out)
+	colnames(out)[1:3]=c("explained_variance","nfeatures","sparsity")
+return(out)}
+
+#for every omic
+temp=lapply(pars,function(y)
+lapply(1:20,function(x) params_searcher(traindata,y,1,1)))
+temp=do.call(rbind,lapply(temp,function(x) do.call(rbind,x)))
+colnames(temp)[4]="sparsity"
+temp$sparsity=as.character(temp$sparsity)
+ggplot(temp[temp$omic=="CpGs",],aes(x=sparsity,
+	y=explained_variance,group=sparsity))+geom_boxplot()
+ggplot(temp[temp$omic=="CpGs",],aes(x=sparsity,
+	y=nfeatures,group=sparsity))+geom_boxplot()
+
+
+####################what happens with 
