46fb40da0e9d7ccce65ce4c726d6e77e3066f4c4
SantiagoDomiro <dopretocoracao@gmail.com>
2021-07-01 13:41:29 -0500

    parallelized per combination of sparsity values

--- sgcca.R
+++ sgcca.R
@@ -1,51 +1,71 @@
-library(data.table)
+#!/usr/bin/env Rscript
+
 library(mixOmics)
+library(data.table)
 
-raw=fread("Her2.mtrx")
-raw=as.matrix(raw[,2:ncol(raw)],rownames=raw$V1)
-raw=apply(cbind(c(1,393133,410210),c(393132,410209,410813)),1,function(x) raw[x[1]:x[2],])
-normi=fread("Her2.normalized")
-normi=as.matrix(normi[,2:ncol(normi)],rownames=normi$V1)
-normi=apply(cbind(c(1,393133,410210),c(393132,410209,410813)),1,function(x) t(normi[x[1]:x[2],]))
-names(raw)=c("CpGs","transcripts","miRNAs")
-names(normi)=c("CpGs","transcripts","miRNAs")
+#sparsity parameters are chosen for each of the 10 MCCV iterations
+#using an internal 5-fold CV loop: the parameters that minimize the
+#prediction error [Tenenhaus2014]
+params_searcher=function(subtype,lCpG,ltrnscri,lmir,count){
+	size=nrow(subtype$miRNAs)
+	i=sample(1:size,round(size/2))
+	#test in half the available data
+	data=lapply(subtype,function(y) y[i,])
+	#ncomp=1 for training
+	results=wrapper.sgcca(data,penalty=c(lCpG,ltrnscri,lmir),scale=T)
+	#save only the model's size
+	evar=as.data.frame(do.call(rbind,results$explained_variance))
+	features=as.data.frame(do.call(rbind,lapply(results$loadings,
+		function(x) sum(x!=0))))
+	out=cbind(evar,features,results$penalty)
+	out$omic=rownames(out)
+	colnames(out)[1:3]=c("explained_variance","nfeatures","sparsity")
+return(out)}
 
+args=commandArgs(trailingOnly=TRUE)
+subtype=args[1]
+lCpG=as.numeric(args[2])
+ltrnscri=as.numeric(args[3])
+lmir=as.numeric(args[4])
+#load data
+data=fread(paste(subtype,"normalized",sep='.'))
 #op1:cca=wrapper.sgcca(raw,penalty=c(1,.5,.3),ncomp=5,scale=T)
 #op2:cca.n=wrapper.sgcca(normalized,penalty=c(1,.5,.3),ncomp=5,scale=F)
 #op3:cca.n1=wrapper.sgcca(normalized,penalty=c(1,.5,.3),ncomp=5,scale=T)
 #op1 & op3 give the same results BUT op1 ends (cca$crit) faster
 #is raw data better???what if such covergence depends on penalties???
 #selectVar() is the same than loadings != 0, NOT loadings.star != 0
+data=as.matrix(data[,2:ncol(data)],rownames=data$V1)
+#separate omics
+data=apply(cbind(c(1,393133,410210),c(393132,410209,410813)),1,function(x) t(data[x[1]:x[2],]))
+names(data)=c("CpGs","transcripts","miRNAs")
 
-#note: the penalty parameters will need to be tuned
-#sparsity parameters are chosen for each of the 10 MCCV iterations
-#using an internal 5-fold CV loop: the parameters that minimize the
-#prediction error [Tenenhaus2014]
+#grid=seq(.01,.9,length=20)
+#test several combinations
+#temp=do.call(rbind,lapply(grid,function(x) 
+#	do.call(rbind,lapply(grid,function(y) 
+#        do.call(rbind,lapply(grid,function(z) 
+#        	{do.call(rbind,lapply(1:10,function(i) 
+#                params_searcher(data,x,y,z)));
+#        	print(paste(x,y,z,sep=','))}))))))#how far am I?
+#test 1 set of sparsity values
+combi=do.call(rbind,lapply(1:20,function(i) 
+	params_searcher(data,lCpG,ltrnscri,lmir)))
+write.table(combi,paste(subtype,lCpG,ltrnscri,lmir,"params",sep='_'),
+	sep='\t',quote=F,row.names=F)
+####run it for as many combinations u wanna
 
-params_searcher=function(subtype,lCpG,ltrnscri,lmir){
-	size=nrow(subtype$miRNAs)
-	i=sample(1:size,round(size/2))
-	data=lapply(subtype,function(y) y[i,])
-	results=wrapper.sgcca(data,penalty=c(lCpG,ltrnscri,lmir),scale=T)
-	#ncomp=1 for training
-	evar=as.data.frame(do.call(rbind,results$explained_variance))
-	features=as.data.frame(do.call(rbind,lapply(results$loadings,
-		function(x) apply(x,2,function(y) sum(y!=0)))))
-	out=cbind(evar,features,results$penalty)
-	out$omic=rownames(out)
-	colnames(out)[1:3]=c("explained_variance","nfeatures","sparsity")
-return(out)}
-
-#for every omic
-temp=lapply(pars,function(y)
-lapply(1:20,function(x) params_searcher(traindata,y,1,1)))
-temp=do.call(rbind,lapply(temp,function(x) do.call(rbind,x)))
-colnames(temp)[4]="sparsity"
-temp$sparsity=as.character(temp$sparsity)
-ggplot(temp[temp$omic=="CpGs",],aes(x=sparsity,
-	y=explained_variance,group=sparsity))+geom_boxplot()
-ggplot(temp[temp$omic=="CpGs",],aes(x=sparsity,
-	y=nfeatures,group=sparsity))+geom_boxplot()
-
-
-####################what happens with 
+#u can build condor submit files like:
+#sub=readLines("temp")#.sub with field separated by \t
+#sub=unlist(strsplit(sub,"\t"))
+#grid=seq(.01,.9,length=10)
+#combination of sparsity values
+#args=as.character(sapply(grid,function(x) 
+#	sapply(grid,function(y) 
+#		sapply(grid,function(z) paste(x,y,z,sep=' ')))))
+#.sub line per combination
+#args=sapply(1:length(args),function(i)
+#	gsub("0.1 0.1 0.1",args[i],sub[2]))
+#sub=sapply(1:length(args),function(x) 
+#	paste(sub[1],args[x],sub[3],sub[4],sub[5],sub[6],sep='_'))
+#writeLines(sub,"temp")#add the lines on the needed resources
