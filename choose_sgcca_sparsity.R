dee158ab66f4d0c472b3f31bccb08553e9cfa5a9
SantiagoDomiro <dopretocoracao@gmail.com>
2021-07-05 20:00:24 -0500

    sparsity of an omic affects selection of the other omics?

--- choose_sgcca_sparsity.R
+++ choose_sgcca_sparsity.R
@@ -8,6 +8,7 @@ files=list.files()
 params=lapply(files,read.table,header=T)
 grid=seq(.01,.9,length=10)#this was the grid used for sgcca.R
 
+####################DESCRIPTION OF DATA##########################
 temp=do.call(rbind,params)
 omics=levels(temp$omic)
 sapply(omics,function(x) 
@@ -17,9 +18,40 @@ sapply(omics,function(x)
 #      0.7537963       0.2520860       0.3010846 
 #each has p-value < 2.2e-16 
 #evar can only guide sparsity selection for CpGs
-# cor.test(temp$explained_variance[temp$omic=="transcripts"],temp$sparsity[temp$omic=="CpGs"],method="spearman")
+#cor.test(temp$explained_variance[temp$omic=="transcripts"],temp$sparsity[temp$omic=="CpGs"],method="spearman")
 #S = 1.9659e+12, p-value < 2.2e-16
 
+########sparsity of an omic affects selection of the other omics?
+#separate per miR sparsity
+i=as.character(sapply(grid,function(x) sapply(grid,function(y) paste("Her2",x,y,sep='_'))))
+temp=lapply(i,function(x) params[grep(x,files)])
+#effect of varying miR sparsity on transcript selection
+table(unlist(lapply(temp,function(x) sapply(1:9,function(y) sapply((y+1):10,function(z) wilcox.test(x[[y]]$nfeatures[x[[y]]$omic=="transcripts"],x[[z]]$nfeatures[x[[z]]$omic=="transcripts"],paired=T)$p.val))))<0.05,useNA="always")
+#FALSE  TRUE  <NA> 
+# 3860   189   451 
+#most comparisons are not significant, miRNA sparsity has little effect
+#effect of varying miRNA sparsity on CpG selection
+table(unlist(lapply(temp,function(x) sapply(1:9,function(y) sapply((y+1):10,function(z) wilcox.test(x[[y]]$nfeatures[x[[y]]$omic=="CpGs"],x[[z]]$nfeatures[x[[z]]$omic=="CpGs"],paired=T)$p.val))))<0.05,useNA="always")
+#FALSE  TRUE  <NA> 
+# 3572   478   450
+#miR sparsity affects more CpG selection than transcript selection
+
+#separate per CpG sparsity
+i=as.character(sapply(grid,function(x) sapply(grid,function(y) paste(x,y,"params",sep='_'))))
+temp=lapply(i,function(x) params[grep(x,files)])
+#effect of varying CpG sparsity on transcript selection
+table(unlist(lapply(temp,function(x) sapply(1:9,function(y) sapply((y+1):10,function(z) wilcox.test(x[[y]]$nfeatures[x[[y]]$omic=="transcripts"],x[[z]]$nfeatures[x[[z]]$omic=="transcripts"],paired=T)$p.val))))<0.05,useNA="always")
+#FALSE  TRUE  <NA> 
+# 3521   529   450 
+#CpG sparsity effect is larger than miR sparsity effect
+#effect of varying CpG sparsity on miRNA selection
+table(unlist(lapply(temp,function(x) sapply(1:9,function(y) sapply((y+1):10,function(z) wilcox.test(x[[y]]$nfeatures[x[[y]]$omic=="miRNAs"],x[[z]]$nfeatures[x[[z]]$omic=="miRNAs"],paired=T)$p.val))))<0.05,useNA="always")
+#FALSE  TRUE  <NA> 
+# 2867   729   904 
+#CpG sparsity affects more miR selection than transcript selection
+######################separate per transcript sparsity
+
+####################CHOOSING SPARSITY##########################
 #this ignores the sparsity value of non-ploted omics
 plotit=function(data,level){
 	data$sparsity=as.character(data$sparsity)
@@ -42,29 +74,6 @@ dev.off()
 i=grep("Her2_0.4",files)
 pre=params[i]
 names(pre)=files[i]
-
-#sparsity of an omic affects selection of the other omics?
-#separate per miR sparsity
-pre_transcri=lapply(paste(grid,"params",sep='_'),function(x)
-	do.call(rbind,pre[grep(x,names(pre))]))
-#separate per transcript sparsity
-pre_miR=lapply(paste("Her2",grid[5],grid,sep='_'),function(x)
-	do.call(rbind,pre[grep(x,names(pre))]))
-#test the effect of miR sparsity on transcript selection
-sapply(2:10,function(x) wilcox.test(pre_transcri[[1]]$
-	nfeatures[pre_transcri[[1]]$omic=="transcripts"],
-	pre_transcri[[x]]$nfeatures[pre_transcri[[x]]$
-	omic=="transcripts"],paired=T)$p.val)
-#[1] 0.347945312 0.918818860 0.830158135 0.300657208 0.992860002
-#[6] 0.664240708 0.308104697 0.009011566 0.362862150
-#miR sparsity has no effect on transcript selection
-sapply(2:10,function(x) wilcox.test(pre_miR[[1]]$
-	nfeatures[pre_miR[[1]]$omic=="transcripts"],
-	pre_miR[[x]]$nfeatures[pre_miR[[x]]$
-	omic=="transcripts"],paired=T)$p.val)
-#[1] 1.426783e-34 1.443684e-34 1.445942e-34 1.446452e-34 1.446655e-34
-#[6] 1.446635e-34 1.446655e-34 1.007779e-34 3.719484e-36
-#but transcript sparsity DOES affect miRNA selection
 
 temp=do.call(rbind,pre)
 #cor.test(temp$explained_variance[temp$omic=="miRNAs"],temp$sparsity[temp$omic=="transcripts"],method="spearman")
@@ -90,8 +99,3 @@ sapply(omics,function(x) colMeans(temp[temp$omic==x,1:2]))
 #                           CpGs       miRNAs  transcripts
 #explained_variance 1.372375e-01   0.05925499 6.288162e-02
 #nfeatures          9.947385e+04 374.25000000 2.878450e+03
-
-
-#será que la omica más grande determina a la que le sigue???
-#cor(cpg sparsity, transcript evar)
-lapply(omics[2:3],function(x) cor.test(temp$explained_variance[temp$omic==x],temp$sparsity[temp$omic==x],method="spearman"))
