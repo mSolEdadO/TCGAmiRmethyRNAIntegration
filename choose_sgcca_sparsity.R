bd913bd7178f5e8ef2301b353a90d9b5e35db0e8
SantiagoDomiro <dopretocoracao@gmail.com>
2021-07-11 18:44:52 -0500

    final vs end loadings works

--- choose_sgcca_sparsity.R
+++ choose_sgcca_sparsity.R
@@ -10,7 +10,7 @@ library(pbapply)
 grid=seq(0,.9,.1)
 temp=pblapply(grid,function(x) 
 	wrapper.sgcca(data,penalty=c(x,1,1),scale=T))
-#take model descriptors
+#take model descriptors<-----------------recycled
 describe=function(resus,names){
 	AVE=as.data.frame(sapply(resus,function(x) 
 		do.call(rbind,x$AVE[[1]])))
@@ -29,6 +29,7 @@ describe=function(resus,names){
 	toplot$omic=factor(toplot$omic,levels=c("CpGs","transcripts","miRNAs"))
 	toplot$sparsity=as.numeric(toplot$sparsity)
 return(toplot)}
+
 results=list()
 results$CpGs=describe(temp,grid)
 results$CpGs=results$CpGs[results$CpGs$omic=="CpGs",]
@@ -43,16 +44,16 @@ results$miRNAs=describe(temp,grid)
 results$miRNAs=results$miRNAs[results$
 	miRNAs$omic=="miRNAs",]
 results=do.call(rbind,results)
-	#plot
+
 plots=lapply(levels(results$omic),function(i) 
 	ggplot(results[results$omic==i,],aes(x=nfeatures,
 		y=AVE,color=sparsity))+geom_point()+ggtitle(i)+
 	theme(text=element_text(size=18)))
-#png("sparsity_search.png")
+png("sparsity_search.png")
  grid.arrange(plots[[1]],plots[[2]],plots[[3]])
  #no facet_wrap coz u want indy axes
 dev.off()
-#turn it analytical by choosing the point when the slop changes
+#turn it analytical by choosing the point when the slop changes????
 sapply(10:2,function(x) (description$AVE[x]-description$AVE[x-1])/
 	(description$nfeatures[x]-description$nfeatures[x-1]))
 #[1] 4.265165e-08 3.532026e-08 3.315587e-08 4.461965e-08 5.877692e-08
@@ -67,8 +68,11 @@ sapply(30:22,function(x) (results$AVE[x]-results$AVE[x-1])/(results$nfeatures[x]
 #[6]  1.169444e-05  2.834565e-05  4.721406e-05  2.599923e-04
 #sparsity: 0.5-0.6 for miRNAs
 
-###############3######CHECK LOADINGS WITH NO SPARSITY
-loadings=as.data.frame(do.call(rbind,results[[6]]$loadings))
+#####################CHECK LOADINGS
+init=wrapper.sgcca(data,penalty=c(1,1,1),scale=T)
+final=wrapper.sgcca(data,penalty=c(0.2,0.6,0.5),scale=T)
+
+loadings=as.data.frame(do.call(rbind,init$loadings))
 loadings$omic=substr(rownames(loadings),1,1)
 loadings$omic=gsub("E","transcripts",gsub("h","miRNAs",
 	gsub("c","CpGs",loadings$omic)))
@@ -80,54 +84,35 @@ png("loadings_l1.png")
 dev.off()
 #transcript & miRNA loadings are normally distributed
 shapiro.test(sample(loadings$comp1[loadings$omic=="transcripts"],5000))
-#W = 0.99956, p-value = 0.3302
+#W = 0.99949, p-value = 0.1937
 shapiro.test(loadings$comp1[loadings$omic=="miRNAs"])
 #W = 0.99848, p-value = 0.8877
 shapiro.test(sample(loadings$comp1[loadings$omic=="CpGs"],5000))#size must be between 3 and 5000
 #W = 0.98626, p-value < 2.2e-16
-#at sparsity=0, 1 feature is selected per omic
-conserved=sapply(results[[1]]$loadings,function(x) rownames(x)[x!=0])
-temp=as.data.frame(sapply(results,function(x) sapply(1:3,function(y) 
-	x$loadings[[y]][rownames(x$loadings[[y]])==conserved[y]])))
-colnames(temp)=grid
-temp$feature=conserved
-temp=temp%>%pivot_longer(-7,names_to="sparsity",values_to="loading")
-png("conserved_features.png")
- ggplot(temp,aes(x=sparsity,y=loading,group=feature))+
- geom_point(aes(color=feature))+theme(text=element_text(size=18))
-dev.off()
+###final loadgings all have p-value < 2.2e-16
 
-#seq(0.05,0.7,length=6)
-grid=seq(0.05,0.7,0.075)#less points aren't useful for miRNAs
-results1=pblapply(grid,function(x) 
-	wrapper.sgcca(data,penalty=c(0.2,x,x),scale=T))
-description=describe(results1,grid)
-sapply(27:20,function(x) (description$AVE[x]-description$AVE[x-1])/(description$nfeatures[x]-description$nfeatures[x-1]))
-#[1] 8.882339e-07 6.234892e-07 4.755818e-07 4.118815e-07 3.965331e-07
-#[6] 4.858871e-07 9.089003e-07 3.766163e-06
-#0.05 for transcripts
-sapply(18:11,function(x) (description$AVE[x]-description$AVE[x-1])/(description$nfeatures[x]-description$nfeatures[x-1]))
-#[1] -3.586748e-06 -2.357042e-06 -1.262106e-06 -1.070951e-06  1.677122e-06
-#[6]  1.162186e-05  1.760406e-05  4.030400e-04
-#0.2 for miRNAs
-plots=lapply(levels(description$omic),function(i) 
-	ggplot(description[description$omic==i,],aes(x=nfeatures,
-		y=AVE,color=sparsity))+geom_point()+ggtitle(i)+
+################################COMPARE INIT & FINAL
+#%of selected features
+100*sapply(final$loadings,function(x) 
+	sum(x!=0))/sapply(init$loadings,function(x) sum(x!=0))
+#       CpGs transcripts      miRNAs 
+#   6.187235   63.565029   46.026490 
+#% of AVE from initial
+100*do.call(rbind,final$AVE[[1]])/do.call(rbind,init$AVE[[1]])
+#                comp1
+#CpGs         80.10103
+#transcripts  96.51604
+#miRNAs      103.08098
+#####whats with miRNAs?????
+temp=as.data.frame(do.call(rbind,lapply(1:3,function(x) 
+	cbind(init$loadings[[x]],final$loadings[[x]]))))
+colnames(temp)=c("initial","final")
+temp$omic=substr(rownames(temp),1,1)
+temp$omic=gsub("E","transcripts",gsub("h","miRNAs",
+ gsub("c","CpGs",loadings$omic)))
+plots=lapply(unique(temp$omic),function(x) 
+	ggplot(temp[temp$omic==x,],aes(x=initial,y=final))+geom_point()+
 	theme(text=element_text(size=18)))
-png("sparsity_search1.png")
+png("loadings_change.png")
  grid.arrange(plots[[1]],plots[[2]],plots[[3]])
 dev.off()
-#plot suggest transcript sparsity 0.05-0.275
-#			  miRNAs       		  0.2-0.275
-#according to slopes
-final=wrapper.sgcca(data,penalty=c(0.2,0.05,0.2),scale=T)
-#according to plots
-finalAlt=wrapper.sgcca(data,penalty=c(0.2,0.2,0.275),scale=T)
-#shapiro.test for finalAlt
-#p-value < 2.2e-16
-
-plots=lapply(unique(temp1$omic),function(i) 
- ggplot(temp1[temp1$omic==i,],aes(x=nfeatures,
- y=AVE,color=sparsity))+geom_point()+ggtitle(i)+
- theme(text=element_text(size=18)))
-grid.arrange(plots[[1]],plots[[2]],plots[[3]])
