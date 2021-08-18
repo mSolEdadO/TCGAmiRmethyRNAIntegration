68b6b88a3218059735380b0281fc500d6b64beea
SantiagoDomiro <dopretocoracao@gmail.com>
2021-08-18 14:33:29 -0500

    commented

--- MInetBP/BP.R
+++ MInetBP/BP.R
@@ -4,14 +4,17 @@
 library(data.table)
 library(biomaRt)
 
-BP=commandArgs(trailingOnly=TRUE)
-
+BP=commandArgs(trailingOnly=TRUE)#GO:XXX of interest
 transcripts=fread("Her2.transcripts")
+
+#get transcripts linked to BP
 mart=useEnsembl("ensembl",dataset="hsapiens_gene_ensembl")
+#biomaRt version fails a lot, so it dies when output aint achieved
 if(length(mart)==0){stop("biomaRt didn't work")}
 geneSet<- getBM(attributes=c('ensembl_gene_id', 'go_id'),
 	filters = 'go',values = BP, mart = mart)
 geneSet=unique(geneSet$ensembl_gene_id[geneSet$go_id==BP])
+#matrix with the transcripts of interest
 transcripts=transcripts[transcripts$probes%in%geneSet,]
 transcripts$probes=paste("BP",transcripts$probes,sep='-')
 write.table(transcripts,file=BP,sep='\t',quote=F,row.names=F)
--- MInetBP/submit_file.R
+++ MInetBP/submit_file.R
@@ -1,13 +1,19 @@
 #!/usr/bin/env Rscript
 
-BP=commandArgs(trailingOnly=TRUE)
+BP=commandArgs(trailingOnly=TRUE)#GO:XXX of interest
 
-targets=readLines("bp.txt")
-sub=readLines("condor.seed")
+targets=readLines("bp.txt")#transcripts linked to BP
+sub=readLines("condor.seed")#executable=/usr/bin/aracne	arguments= -i data/GO.txt -h ENSG -p 0.000001	output=GO.ENSG.output	error=GO.ENSG.error	log=GO.ENSG.log	queue
+#-p values attempted to keep all BP nodes, check for larger BP
 
+#update to BP
 sub=gsub("GO",BP,sub)
+#to identify transcripts linked to BP
 targets=paste("BP",targets,sep='-')
+#get condor lines for each transcript in the BP
 sub1=sapply(targets,function(x) gsub("ENSG",x,sub))
+#get condor lines for the matrix with CpGs
 sub2=gsub(BP,paste(BP,"cpgs",sep='-'),sub1)
+#more restrictive -p so u get less edges
 sub2=gsub("-p 0.001","-p 0.000001",sub2)
 writeLines(c(sub1,sub2),paste(BP,"sub",sep='.'))
