1ce42a64ab267c9ae4f70f12c0d9fda564c7e83e
SantiagoDomiro <dopretocoracao@gmail.com>
2021-08-19 14:59:04 -0500

    drafted subgraphs per BP

---
+++ sgcca/BPnet.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+library(data.table)
+library(igraph)
+library(biomaRt)
+
+BP=commandArgs(trailingOnly=TRUE)
+edges=fread(paste(BP,"net",sep='.'))
+
+########################focus on BP nodes##################
+mart=useEnsembl("ensembl",dataset="hsapiens_gene_ensembl")
+print(paste("Checking",BP,"in biomaRt"))#this takes a loooong time
+geneSet<- getBM(attributes=c('ensembl_gene_id', 'go_id'),
+	filters = 'go',values = BP, mart = mart)
+#using BP as filter outputs other GOs eitherway
+geneSet=unique(geneSet$ensembl_gene_id[geneSet$go_id==BP])
+#verify BP nodes are in col2
+print(sum(edges$source%in%geneSet))
+print(sum(edges$target%in%geneSet))
+edges=edges[edges$target%in%geneSet,]
+
+########################collapse CpGs per CGI##################
+methy=fread("jhu-usc.edu_BRCA.HumanMethylation450.6.lvl-3.TCGA-AO-A03N-01B-11D-A10N-05.gdc
+_hg38.txt")
+colnames(methy)[1]="ID"
+
+#verify CpGs are in col1
+print(table(substr(edges$source,1,1)))
+print(table(substr(edges$target,1,1)))
+#separate edges with & without CpGs
+methy=methy[methy$ID%in%edges$source,]
+temp=edges[substr(edges$source,1,1)=="c",]
+edges=edges[substr(edges$source,1,1)!="c",]
+#add CGI info
+temp=temp[order(temp$source),]
+i=table(temp$source)
+temp$CGI=unlist(sapply(1:2979,function(x) 
+	rep(methy$CGI_Coordinate[methy$ID==names(i)[x]],i[x])))
+
+g=graph.data.frame(temp[,c(2,4)],directed=F)#targets to CGI
+E(g)$cor=as.numeric(temp$corr)
+#magic happens here
+g1=simplify(g,edge.attr.comb=list(cor=function(x) 
+	median(abs(x)),name="ignore"))
+edgesAlt=as.data.frame(get.edgelist(g1))
+edgesAlt$corr=E(g1)$cor
+edgesAlt=rbind(as.matrix(edgesAlt),as.matrix(edges))
