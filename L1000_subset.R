b9a9140b72d7b3e437874e19eba328e9a7fbf52a
SantiagoDomiro <dopretocoracao@gmail.com>
2021-10-09 19:10:00 -0500

    commented loaded files

--- L1000_subset.R
+++ L1000_subset.R
@@ -6,15 +6,22 @@ cells=commandArgs(trailingOnly=TRUE)
 
 meta=loadCMapData("cmapMetadata.txt", type="metadata")
 #https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=file&file=GSE92742_Broad_LINCS_sig_info.txt.gz
+#meta=loadCMapData("GSE70138_Broad_LINCS_sig_metrics_2017-03-06.txt",
+#for some reason sig_info files sig_id does NOT match matrix IDs
+#        type="metadata")
+#meta$cell_id=sapply(strsplit(meta$sig_id,"_"),function(x) x[2])
 meta <- filterCMapMetadata(meta,perturbationType="trt_cp")
+#trt_cp = Peptides and other biological agents (e.g. cytokine)
 meta <- filterCMapMetadata(meta,cellLine=cells)
 
 ###################GET SUBSET OF INTEREST
 perturbations <- prepareCMapPerturbations(
     metadata=meta,
+#change paths to load another GSE
     zscores="/labs/csbig/multiomics/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx",
 #Level 5 (SIG) consists of the replicates, usually 3 per treatment, aggregated into a single differential expression vector derived from the weighted averages of the individual replicates.
+#zscores="GSE70138_Broad_LINCS_Level5_COMPZ_n118050x12328_2017-03-06.gctx",
     geneInfo="cmapGeneInfo.txt",compoundInfo="cmapCompoundInfo.txt")
-#change zscores path to load another GSE
+#geneInfo="GSE70138_Broad_LINCS_gene_info_2017-03-06.txt",compoundInfo="cmapCompoundInfo.txt")
 zscores=loadCMapZscores(perturbations)
 write.table(zscores,paste(cells,"mtrx",sep='.'),sep='\t',quote=F)
--- enricheDrugs.R
+++ enricheDrugs.R
@@ -4,23 +4,23 @@ library(cTRAP)
 meta <- loadCMapData("cmapMetadata.txt", type="metadata")
 #https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=file&file=GSE92742_Broad_LINCS_sig_info.txt.gz
 #keep  IDs
-ids=meta[,c("sig_id","pert_iname")]
+ids <- meta[,c("sig_id","pert_iname")]
 meta <- loadCMapData("GSE70138_Broad_LINCS_sig_metrics_2017-03-06.txt",
 		type="metadata")
 ids=rbind(ids,meta[,c("sig_id","pert_iname")])
 drugs <- loadCMapData("cmapCompoundInfo.txt", type="compoundInfo")
 
 #LOAD ENRICHMENT
-files=list.files()
-files=files[grep("gsea",files)]
-data=lapply(files,read.table,header=T)
-names(data)=gsub(".gsea","",files)
+files<-list.files()
+files<-files[grep("gsea",files)]
+data<-lapply(files,read.table,header=T)
+names(data)<-gsub(".gsea","",files)
 
 #ADD METADATA TO ENRICHED SIGNATURES
 data=lapply(data,function(x) merge(x,ids,by="sig_id"))
 data=lapply(data,function(x) merge(x,drugs,by="pert_iname"))
 data=lapply(data,function(x) x[order(x$NES,decreasing=T),])
-#negative NES would be drugs that should not be used, right?
+
 #KEEP ONLY LAUNCHED DRUGS
 launched=lapply(data,function(x) x[x$clinical_phase=="Launched",])
 #separate per series and tissue
@@ -34,20 +34,22 @@ write.table(do.call(rbind,launched[names(launched)%in%cells]),
 #lung results
 write.table(do.call(rbind,launched[!names(launched)%in%cells]),
 	"LungEnricheDrugs",sep='\t',quote=F,row.names=F)
-########################
+
+
+########################ARE COMMUNITIES THE SAME ACROSS NETS???
 library(biomaRt)
 library(HTSanalyzeR)
 
 mart=useEnsembl("ensembl",dataset="hsapiens_gene_ensembl",host="http://apr2019.archive.ensembl.org")
 myannot=getBM(attributes = c("ensembl_gene_id","hgnc_symbol","chromosome_name","mirbase_id","external_gene_name","entrezgene"), mart=mart)
-universo=as.character(myannot$hgnc_symbol[!is.na(myannot$entrezgene)])
+#used ensembl ids since communities original ids are of this type
+universo=as.character(myannot$ensembl_gene_id)
 
 communities=read.table("communities.tsv",sep='\t',header=T)
 communities$group=gsub("community[0-9]+","",communities$name,perl=T)
 sets=lapply(unique(communities$group),function(x) communities[communities$group==x,])
 sets=lapply(sets,function(x) 
 	lapply(unique(x$name),function(y) x$genes[x$name==y]))
-library(HTSanalyzeR)
 
 sameSet=temp=lapply(1:3,function(x) lapply((x+1):4,function(z) 
 	do.call(rbind,lapply(sets[[x]],function(y)
@@ -56,11 +58,9 @@ sameSet=temp=lapply(1:3,function(x) lapply((x+1):4,function(z)
 	 	pAdjustMethod="fdr")))))
 #modules contain the same genes
 sapply(sameSet,function(x) sapply(x,function(y) sum(y[,7]<0.05)))
-[[1]]
-[1] 23 24 24
-
-[[2]]
-[1] 25 23
-
-[[3]]
-[1] 22
+#[[1]]
+#[1] 23 24 24
+#[[2]]
+#[1] 25 23
+#[[3]]
+#[1] 22
