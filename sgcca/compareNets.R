c32040a54d5b796afb4015e67da9810333b66da0
SantiagoDomiro <dopretocoracao@gmail.com>
2022-03-10 14:56:44 -0600

    plotted shared edges

--- sgcca/compareNets.R
+++ sgcca/compareNets.R
@@ -7,7 +7,6 @@ library(tidyverse)
 library(igraph)
 library(ape)
 library(ggplot2)
-library(biomaRt)
 
 #########################LOAD NETS
 files=list.files()
@@ -63,37 +62,80 @@ print(paste(names(which(ps<0.01)),
 #print(temp$residuals)
 #but this way u dont get a p.value per edge type
 
+#########################EXCLUSIVE EDGES
+exclusive=edges[!edges$i%in%edges$i[duplicated(edges$i)],]
+#net with exclusive edges
+exclusive=exclusive%>%group_by(subtype)%>%
+			group_map(~graph.data.frame(.x[,c("X1","X2")]))
+dg=sapply(exclusive,degree)
+names(dg)=sapply(strsplit(files,".",fixed=T),function(x) x[1])
+#df to plot
+temp=as.data.frame(do.call(rbind,sapply(1:5,function(x) 
+	cbind(subtype=names(dg)[x],g=dg[[x]]))))
+png(paste(fun,"degree.png",sep='_'))
+ggplot(temp,aes(x=as.numeric(g),col=subtype,fill=subtype))+
+	geom_density(aes(y=..scaled..),alpha=0.3)+
+	scale_x_continuous(trans="log10")+xlab("degree")+
+	theme(text=element_text(size=18))
+dev.off()
+#check high degree nodes
+temp=sapply(dg,function(x) names(which(x>mean(x))))#1:present,0:absent
+temp=apply(temp,1,paste,collapse="")#0,1 chains ordered as the nets
+if(sum(temp=="11110")){
+	print(paste(names(which(temp=="11110")),
+	"is a high degree node, shared by the 4 subtype networks"))
+}
+if(sum(temp=="11111")){
+	print(paste(names(which(temp=="11111")),
+	"is a high degree node, shared by the 5 networks"))
+}
+
 #########################INTERSECT NETS
 shared=intersection(nets$Basal,
 					nets$Her2,
 					nets$LumA,
 					nets$LumB,
 					nets$Normal)
-n=5
 if(ecount(shared)==0){
-	shared=intersection(nets$Basal,
-						nets$Her2,
-						nets$LumA,
-						nets$LumB)
-	n=4
-}
+	stop("No shared edges")
+	}
+n=5
 shared=induced_subgraph(shared,which(degree(shared)>0))#drop loose nodes
-plot(shared,vertex.color="gray",vertex.label.cex=1.5,
-	vertex.frame.color=NA,vertex.label.family="sans",
-	vertex.label.color="black",edge.width=3)
 
 #DO WEIGHTS CHANGE ACROSS SUBTYPES?
 ws=edge_attr(shared)
 ws=as.data.frame(do.call(cbind,ws[1:n]))
 colnames(ws)=names(nets)[1:n]
-ws$edge=1:nrow(ws)
+#ws$edge=1:nrow(ws)
 #png(paste(fun,"weights.png",sep='.'))
-ws%>%pivot_longer(-edge,names_to="subtype",values_to="weight")%>%
-	ggplot(aes(subtype,edge,fill=weight))+geom_tile()+
-	scale_fill_viridis_c()+ylab("")+xlab("")+
-	theme(text=element_text(size=18),axis.ticks.y=element_blank(),
-		axis.text.y=element_blank())
+#ws%>%pivot_longer(-edge,names_to="subtype",values_to="weight")%>%
+#	ggplot(aes(subtype,edge,fill=weight))+geom_tile()+
+#	scale_fill_viridis_c()+ylab("")+xlab("")+
+#	theme(text=element_text(size=18),axis.ticks.y=element_blank(),
+#		axis.text.y=element_blank())
 #dev.off()
+
+#plot
+load("../../Downloads/DA.RData")
+DE.miR=lapply(DE.miR,function(x) x[rownames(x)%in%V(shared)$name,])
+DM.cpg1=lapply(DM.cpg1,function(x) x[rownames(x)%in%V(shared)$name,])
+DE.miR=do.call(rbind,lapply(1:4,function(x) cbind(contrast=names(DE.miR)[x],DE.miR[[x]],node=rownames(DE.miR[[x]]))))
+DM.cpg1=do.call(rbind,lapply(1:4,function(x) cbind(contrast=names(DM.cpg1)[x],DM.cpg1[[x]],node=rownames(DM.cpg1[[x]]))))
+cols=rbind(DE.miR,DM.cpg1)
+cols=cols[order(match(cols$node,V(shared)$name)),]
+lay=layout.auto(shared)
+png(paste(fun,"shared_net.png",sep='_'))
+par(mfrow=c(2,2))
+sapply(1:4,function(x) {
+	plot(shared,
+	vertex.color=rev(RColorBrewer::brewer.pal(name="RdBu",n=10))[cols$t[cols$contrast==unique(cols$contrast)[x]]],
+	vertex.label.cex=1.5,layout=lay,vertex.size=30,
+	vertex.label.family="sans",main=names(nets)[x],
+	vertex.label.color="black",
+	edge.label=c("","-")[factor(as.numeric(ws[,x])<0,levels=c("FALSE","TRUE"))],
+	edge.label.cex=3,
+	edge.width=10*abs(as.numeric(ws[,x])))})
+dev.off()
 #WHAT MAKES SPECIAL THIS EDGES?????????
 
 
@@ -103,6 +145,7 @@ ws%>%pivot_longer(-edge,names_to="subtype",values_to="weight")%>%
 #↓
 #↓
 #########################ADD KNOWN REGULATORY EDGES 
+#library(biomaRt)
 #files=list.files()
 #files=files[grep(fun,files)]
 #files=files[grep("moti",files)]
