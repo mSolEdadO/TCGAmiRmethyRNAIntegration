76591823717833a667f2894d6e9458131e9925bd
SantiagoDomiro <dopretocoracao@gmail.com>
2022-03-30 14:10:48 -0600

    updated for any function/subty

--- sgcca/get_matrix.R
+++ sgcca/get_matrix.R
@@ -1,41 +1,29 @@
-#single dataframe with the 4 subtypes & the normal tissue
-sets=do.call(rbind,lapply(1:5,function(x) 
-	cbind(subtype=names(sets)[x],
-		sets[[x]][,c("component","variable")])))
+#!/usr/bin/env Rscript
+library(tidyverse)
 
-#get functions enriched in the 4 subtypes & the normal tissue
-shared=sapply(functions,function(x) 
-	names(which(table(unlist(x))==5)))
-enriched=lapply(1:2,function(x) 
-	enriched[[x]][enriched[[x]]$ID%in%shared[[x]],])
-names(enriched)=names(shared)
-#drop child terms 
-todrop=c("GO:0007156",#GO:0007156 is a GO:0098742
-	"GO:0072009",#is a GO:0072006 & GO:0072073                              
-	"GO:0072080")#is a GO:0061326 & GO:0072009
-enriched$BP=enriched$BP%>%filter(!ID%in%todrop)
+########################PARAMETERS & PACKAGES
+args=commandArgs(trailingOnly=TRUE)
+fun=args[1]
+subty=args[2]
 
-#get features for selected functions
-features=lapply(enriched,function(x) 
-	x%>%dplyr::select(subtype,component,ID)%>%
-	merge(sets,by=c("subtype","component")))
+#get the components linked to the function
+if(grep("GO",fun)){
+ enrich=read_tsv("BP-allFeatures.enrichment")
+}else{
+ enrich=read_tsv("KEGG-allFeatures.enrichment")	
+}
+comp=enrich%>%filter(ID==fun&subtype==subty)%>%
+			  dplyr::select(component)%>%unlist
 
-#get features matrix per function & subtype
-files=list.files()
-files=files[grep("eigeN",files)]
-data=lapply(files,function(x) data.table::fread(x))
-names(data)=gsub(".eigeNormi","",files)
-data=lapply(data,function(x) 
-	as.matrix(x[,2:ncol(x)],rownames=x$V1))
-data=lapply(features,function(z) 
-	lapply(unique(z$ID),function(x) 
-		lapply(unique(z$subtype),function(y) 
-			data[[y]][rownames(data[[y]])%in%
-				z$variable[z$ID==x&z$subtype==y],])))
-lapply(1:2,function(z) 
-	lapply(1:length(unique(features[[z]]$ID)),function(x) 
-		lapply(1:5,function(y) 
-				write.table(data[[z]][[x]][[y]],
-				paste(unique(features[[z]]$subtype)[y],
-					unique(features[[z]]$ID)[x],sep='.'),
-				sep='\t',col.names=F,quote=F))))
+#get the features selected in those components
+selected=read_tsv(paste(subty,"selected",sep='.'))
+features=selected%>%filter(component==unlist(comp))%>%
+		 distinct(variable)%>%unlist
+print(paste("Function has",nrow(features),"features associated",sep=' '))
+
+#get the data
+data=data.table::fread(paste(subty,"eigeNormi",sep='.'))
+data=data[data$V1%in%features,]
+data=as.matrix(data[,2:ncol(data)],rownames=data$V1)
+write.table(data,paste(fun,subty,"mtrx",sep='.'),sep='\t',quote=F)
+
